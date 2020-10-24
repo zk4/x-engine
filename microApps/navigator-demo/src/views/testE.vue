b4ad8b414da7aa5f6917af80cb693fbab4e870c4
zk <liuzq7@gmail.com>
2020-10-24 20:27:18 +0800

    chores: add more demo for microapps

---
+++ microApps/navigator-demo/src/views/testE.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div style="margin: 16px;">
+      <div>current page: testE</div>
+    </div>
+    <div style="margin: 16px;">
+      <div>原生跳转</div>
+    </div>
+    <div style="margin: 16px;">
+      <van-button type="primary" round block @click="back">上一页</van-button>
+    </div>
+    <div style="margin: 16px;">
+      <van-button type="primary" round block @click="backToB">返回testB页面</van-button>
+    </div>
+    <div style="margin: 16px;">
+      <van-button type="primary" round block @click="backRoot">返回原生页面</van-button>
+    </div>
+    <div style="margin: 16px;">原生跳转携带参数为: {{this.params}}</div>
+    <div style="margin: 16px;">
+      <div>h5内部跳转</div>
+    </div>
+    <div style="margin: 16px;">
+      <van-button type="primary" round block @click="h5back">上一页</van-button>
+    </div>
+    <div style="margin: 16px;">携带参数为: {{this.params}}</div>
+  </div>
+</template>
+
+<script>
+import nav from "@zkty-team/x-engine-module-nav";
+export default {
+  name: "testE",
+  data() {
+    return {
+      params: "",
+    };
+  },
+  mounted() {
+    this.params = this.$route.query.qid;
+  },
+  methods: {
+    back() {
+      nav.navigatorBack();
+    },
+
+    backToB() {
+      nav.navigatorBack({
+        path: "/testB",
+      });
+    },
+
+    backRoot() {
+      nav.navigatorBack({
+        path: "0",
+      });
+    },
+
+    h5back() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>

c12e4424e58c772c71dccddaf488461eb5f6301e
zk <liuzq7@gmail.com>
2020-10-24 18:40:22 +0800

    add:  convert dict tot query like string

---
+++ modules/x-engine-module-router/iOS/Class/NSDictionary+UrlEncoding.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+UrlEncoding.m
+//  router
+//
+//  Created by zk on 2020/10/24.
+//  Copyright © 2020 zk. All rights reserved.
+//
+
+#import "NSDictionary+UrlEncoding.h"
+
+@implementation NSDictionary_UrlEncoding
+
+@end

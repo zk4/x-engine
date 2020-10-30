78ff605450a343f73a5a7f24951444764fb83fd1
李宫 <gong11.li@nttdata.com>
2020-10-30 18:32:42 +0800

    update

--- modules/x-engine-module-camera/model.ts
+++ modules/x-engine-module-camera/model.ts
@@ -35,16 +35,20 @@ function openImagePicker(
     camera
       .openImagePicker({
         __event__: (res) => {
-          console.log(res);
-          //拼接参数，例：'?w=200&h=100&q=0.5&bytes=1024'
-          document.getElementById("debug_text").innerText = res;
+          // console.log(res);
+          //如果获取链接，可以拼接参数，例：'?w=200&h=100&q=0.5&bytes=1024'
+          
+          // document.getElementById("debug_text").innerText = res;
 
-          // var tag = document.getElementsByClassName('photo')[0];
-          // if(tag){
-          //   tag.setAttribute('src', res);
-          // }else{
-          //   document.body.innerHTML += "<img class='photo' style='width: 100%' "+"src="+res+">";
-          // }
+          var tag = document.getElementsByClassName('photo')[0];
+          if(tag){
+            // tag.setAttribute('src', res);
+            tag.setAttribute('src', "data:image/png;base64,"+res);
+          }else{
+            // document.body.innerHTML += "<img class='photo' style='width: 100%' "+"src="+res+">";
+            document.body.innerHTML += "<img class='photo' style='width: 100%' "+"src="+"'data:image/png;base64,"+res+"'"+">";
+
+          }
         },
       })
       .then((res) => {
@@ -52,3 +56,5 @@ function openImagePicker(
       });
   };
 }
+
+

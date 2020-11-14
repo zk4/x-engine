4d2b187b6679bc0d4978dbbe2a89fe059263fdee
zk <liuzq7@gmail.com>
2020-11-15 01:51:20 +0800

    fix:  using xengine  lifecycle

--- modules/x-engine-module-dcloud/iOS/Class/__xengine__module_dcloud.m
+++ modules/x-engine-module-dcloud/iOS/Class/__xengine__module_dcloud.m
@@ -18,7 +18,7 @@
 #import "DCUniMP.h"
 #import <MicroAppLoader.h>
 
-@interface __xengine__module_dcloud() <DCUniMPSDKEngineDelegate>
+@interface __xengine__module_dcloud() <UIApplicationDelegate>
 @property (nonatomic, weak) DCUniMPInstance *uniMPInstance; /**< 保存当前打开的小程序应用的引用 注意：请使用 weak 修辞，否则应在关闭小程序时置为 nil */
 @end
 

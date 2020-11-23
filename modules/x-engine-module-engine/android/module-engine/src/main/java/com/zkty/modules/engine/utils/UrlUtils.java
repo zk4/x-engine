93e332b5591fafe0bbf950264251261de292376c
xiefei <xiefei_525@163.com>
2020-11-23 14:06:44 +0800

    fix(appC):appC checkUrl 处理

---
+++ modules/x-engine-module-engine/android/module-engine/src/main/java/com/zkty/modules/engine/utils/UrlUtils.java
@@ -0,0 +1,22 @@
+package com.zkty.modules.engine.utils;
+
+import android.text.TextUtils;
+
+public class UrlUtils {
+
+    public static boolean equalsWithoutArgs(String url1, String url2) {
+        if (TextUtils.isEmpty(url1) || TextUtils.isEmpty(url2)) {
+            return false;
+        }
+
+        if (url1.contains("?")) {
+            url1 = url1.substring(0, url1.charAt('?'));
+        }
+        if (url2.contains("?")) {
+            url2 = url1.substring(0, url2.charAt('?'));
+        }
+
+
+        return url1.equals(url2);
+    }
+}

50e27896db7f3e15d8c0fcdad80e7bf560d1fc8b
zk <liuzq7@gmail.com>
2020-12-04 17:06:36 +0800

    feat(geo):  增加地理位置信息

---
+++ modules/x-engine-module-geo/iOS/Class/__xengine__module_geo.m
@@ -0,0 +1,38 @@
+//
+//  xengine__module_geo.m
+//  geo
+//
+//  Created by zk on 2020/9/7.
+//  Copyright © 2020 edz. All rights reserved.
+
+
+#import "__xengine__module_geo.h"
+#import <XEngineContext.h>
+#import <micros.h>
+#import <UIViewController+.h>
+#import <JSONToDictionary.h>
+#import <RecyleWebViewController.h>
+#import <XEngineWebView.h>
+#import <Unity.h>
+
+@interface __xengine__module_geo()
+@end
+
+@implementation __xengine__module_geo
+ 
+  
+
+- (void)_coordinate:(GeoReqDTO *)dto complete:(void (^)(GeoResDTO *, BOOL))completionHandler {
+    
+}
+
+- (void)_locate:(GeoReverseReqDTO *)dto complete:(void (^)(GeoReverseResDTO *, BOOL))completionHandler {
+     
+}
+
+
+
+
+
+@end
+ 

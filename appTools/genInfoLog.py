eda4f39e17d90770b7adeac77e79781144f4ffc2
zk <liuzq7@gmail.com>
2020-12-28 18:55:09 +0800

    chores(tools): pgy counts

--- appTools/genInfoLog.py
+++ appTools/genInfoLog.py
@@ -7,7 +7,7 @@ bashCommand = "git log --pretty=format:%cn||%s||%cr"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 lines = output.decode('utf-8').split("\n")
-timestoEnds =1
+timestoEnds =3
 for line in lines:
     [name,comment,elapsed] = line.split('||')
     matches = ["fix","feat","feature"]

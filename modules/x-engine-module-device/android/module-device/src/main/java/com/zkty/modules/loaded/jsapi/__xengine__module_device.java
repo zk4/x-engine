d7049c9b3f4c8937e69653632a2ca4faf5b868b8
xiefei <xiefei_525@163.com>
2020-12-30 15:01:02 +0800

    feat(engine):device新增发送短信功能

--- modules/x-engine-module-device/android/module-device/src/main/java/com/zkty/modules/loaded/jsapi/__xengine__module_device.java
+++ modules/x-engine-module-device/android/module-device/src/main/java/com/zkty/modules/loaded/jsapi/__xengine__module_device.java
@@ -1,17 +1,24 @@
 package com.zkty.modules.loaded.jsapi;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.zkty.modules.dsbridge.CompletionHandler;
+import com.zkty.modules.engine.activity.XEngineWebActivity;
 import com.zkty.modules.engine.utils.DensityUtils;
 import com.zkty.modules.engine.utils.DeviceUtils;
 import com.zkty.modules.engine.utils.XEngineWebActivityManager;
 
 public class __xengine__module_device extends xengine__module_device {
+
+    private static int PERMISSION_REQUEST_SMS = 420;
 
     @Override
     public void _getPhoneType(DeviceSheetDTO dto, CompletionHandler<DeviceMoreDTO> handler) {
@@ -131,5 +138,81 @@ public class __xengine__module_device extends xengine__module_device {
         Uri data = Uri.parse("tel:" + dto.phoneNumber);
         intent.setData(data);
         XEngineWebActivityManager.sharedInstance().getCurrent().startActivity(intent);
+    }
+
+    @Override
+    public void _deviceSendMessage(final DeviceMessageDTO dto, CompletionHandler<DeviceMoreDTO> handler) {
+
+        final XEngineWebActivity activity = XEngineWebActivityManager.sharedInstance().getCurrent();
+        XEngineWebActivity.LifecycleListener lifeCycleListener = new XEngineWebActivity.LifecycleListener() {
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onRestart() {
+
+            }
+
+            @Override
+            public void onResume() {
+
+            }
+
+            @Override
+            public void onPause() {
+
+            }
+
+            @Override
+            public void onStop() {
+
+            }
+
+            @Override
+            public void onDestroy() {
+
+            }
+
+            @Override
+            public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+            }
+
+            @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+                if (requestCode == PERMISSION_REQUEST_SMS) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (activity.checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + dto.phoneNumber));
+                            intent.putExtra("sms_body", dto.messageContent);
+                            XEngineWebActivityManager.sharedInstance().getCurrent().startActivity(intent);
+                        }
+                    }
+                }
+
+            }
+        };
+
+
+        if (activity != null) {
+            activity.removeLifeCycleListener(lifeCycleListener);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                activity.requestPermissions(new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_SMS);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + dto.phoneNumber));
+                intent.putExtra("sms_body", dto.messageContent);
+                XEngineWebActivityManager.sharedInstance().getCurrent().startActivity(intent);
+            }
+        }
+
     }
 }

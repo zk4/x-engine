3ba35b1daac972446513f535b0789e468cfffb10
zk <liuzq7@gmail.com>
2021-01-05 13:28:32 +0800

    feat(app): 增加 app 模板

---
+++ modules/x-engine-app/iOS/xxxx/EntryViewController.m
@@ -0,0 +1,31 @@
+//
+//  EntryViewController.m
+//  ModuleApp
+//
+
+#import "EntryViewController.h"
+
+#import <x-engine-module-engine/XEOneWebViewControllerManage.h>
+
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+@interface EntryViewController ()
+
+@end
+
+@implementation EntryViewController
+- (IBAction)Action:(id)sender {
+    [self pushTestModule];
+}
+
+-(void) pushTestModule{
+    [[XEOneWebViewControllerManage sharedInstance] pushWebViewControllerWithHttpRouteUrl:@"http://192.168.1.107:9111"];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self pushTestModule];
+
+}
+
+@end

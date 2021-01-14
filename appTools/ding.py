3df85302a16140eadbd4a13e1e27f4bd8f2d92e0
zk <liuzq7@gmail.com>
2021-01-15 01:35:48 +0800

    chores(tools): send dingding update to python

---
+++ appTools/ding.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import requests
+import sys
+import json
+def genGitLog():
+    bashCommand = "git log --pretty=format:%cn||%s||%cr"
+
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    lines = output.decode('utf-8').split("\n")
+    timestoEnds = 7
+    ret = ""
+    for line in lines:
+        [name,comment,elapsed] = line.split('||')
+        matches = ["fix","feat","feature"]
+        if any( comment.startswith(x) for x in matches) :
+            comment = comment.replace('\"','')
+            comment = comment.replace('/','')
+            ret += "- "+name+comment+" "+ elapsed+'\n'
+        if comment.startswith("pgy"):
+            timestoEnds-=1
+            ret += "- "+'------last version------'+'\n'
+        if timestoEnds==0:
+            break
+    return ret
+    
+
+def sendDingDing(payload:dict,url):
+
+    headers = {
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload.encode('utf-8'))
+    print(response.text)
+
+def getTimeStamp():
+    import datetime
+    import time
+    dtime = datetime.date.today()
+    unix_time = time.mktime(dtime.timetuple())
+    dtime2 = datetime.datetime.fromtimestamp(unix_time)
+    return dtime2
+
+def doaction(name,version,downloadurl,build):
+    text= f"""
+{name} {version} 已上传
+
+
+蒲公英版为快速验证测试版本。仅供验证! 一切功能以 tf 为准。
+蒲公英版本不支持:蓝牙(开门), 推送，有赞，苹果登陆，微信。
+
+
+[{name}]({downloadurl})
+
+
+![qrcode](https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={downloadurl})
+
+**版本信息**
+**Build**: {build}
+**编译时间**: {getTimeStamp()}
+**近期git历史**: 
+{genGitLog()}  
+        """
+    text = "\n".join(text.splitlines())
+    url = "https://oapi.dingtalk.com/robot/send?access_token=55580c4957aa67216e782e6da0d4908ce5e62df9dcac2b4d4950e6098ee502d1"
+    payload = {
+     "msgtype": "markdown",
+     "markdown": {
+         "title":title,
+         "markdown": text 
+     },
+    "at": {
+        "atMobiles": [], 
+        "isAtAll": False
+    }
+ }
+    payload = (json.dumps(payload,ensure_ascii=False))
+    print(payload)
+    sendDingDing(payload,url)
+
+if __name__ == "__main__":
+    # "/Users/zk/git/company/working/dingding.sh" + " " + name + " " + version + " " + build + " " + " " + url  
+    args = sys.argv
+    if(len(args)<5):
+        print("参数不够 .py <title> <version> <build> <downloadurl>")
+        exit() 
+
+    title = args[1]
+    version =args[2]
+    build =args[3]
+    downloadurl =args[4]
+    doaction(title,version, downloadurl,build)

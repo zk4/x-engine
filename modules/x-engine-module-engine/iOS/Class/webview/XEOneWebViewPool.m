6a8e2479199fdca41c051c443d5559f8e453350b
lvdongjian <lvdongjian@vipkid.com.cn>
2021-02-06 18:28:00 +0800

    fix():修改通知发送时机

--- modules/x-engine-module-engine/iOS/Class/webview/XEOneWebViewPool.m
+++ modules/x-engine-module-engine/iOS/Class/webview/XEOneWebViewPool.m
@@ -219,16 +219,24 @@ NSNotificationName const XEWebViewLoadFailNotification = @"XEWebViewLoadFailNoti
     } else if ([keyPath isEqualToString:@"URL"]) {
         
         RecyleWebViewController *vc = (RecyleWebViewController *)[Unity sharedInstance].getCurrentVC;
+        NSURL *oldUrl = [change objectForKey:@"old"];
         if([vc isKindOfClass:[RecyleWebViewController class]]){
-            NSURL *oldUrl = [change objectForKey:@"old"];
+            
             if(oldUrl && ![oldUrl isKindOfClass:[NSNull class]]){
-//                if([vc.loadUrl isEqualToString:[oldUrl absoluteString]]
-//                   || [vc.loadUrl isEqualToString:[oldUrl absoluteString]]){
+                if([vc.loadUrl isEqualToString:[oldUrl absoluteString]]){
+
                     [[NSNotificationCenter defaultCenter] postNotificationName:XEWebViewProgressChangeNotification object:@{
                         @"URL":[change objectForKey:@"new"],
                         @"webView":object,
                     }];
-//                }
+                }
+            }
+        }else{
+            if(oldUrl && ![oldUrl isKindOfClass:[NSNull class]]){
+                [[NSNotificationCenter defaultCenter] postNotificationName:XEWebViewProgressChangeNotification object:@{
+                    @"URL":[change objectForKey:@"new"],
+                    @"webView":object,
+                }];
             }
         }
     } else {

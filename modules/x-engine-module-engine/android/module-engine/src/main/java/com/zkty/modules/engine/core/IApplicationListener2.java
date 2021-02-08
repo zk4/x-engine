b17d821b7dd61d2a0b007a93bada44688d2db4e7
xiefei <xiefei_@525@163.com>
2021-02-08 11:30:53 +0800

    fix(appc):IM模块

---
+++ modules/x-engine-module-engine/android/module-engine/src/main/java/com/zkty/modules/engine/core/IApplicationListener2.java
@@ -0,0 +1,10 @@
+package com.zkty.modules.engine.core;
+
+import android.content.Context;
+
+public interface IApplicationListener2 {
+
+    void onAppCreate(Context context);
+
+    void onTerminate();
+}

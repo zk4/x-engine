7a478abf9c19a67eedeec9575ccabec7e63cfd70
liuxiao00256 <m18291165921@163.com>
2021-03-01 12:05:17 +0800

    fix(app-c):阴影，首页banner崩溃

--- modules/x-engine-module-engine/iOS/Class/webview/RecyleWebViewController.m
+++ modules/x-engine-module-engine/iOS/Class/webview/RecyleWebViewController.m
@@ -370,7 +370,7 @@ static   XEngineWebView* s_webview;
     self.navBarHairlineImageView = [self findHairlineImageViewUnder:self.navigationController.navigationBar];
     self.navBarHairlineImageView.hidden = YES;
 //
-    [self.navigationController.navigationBar setLayerShadow:[UIColor colorWithRed:0/255.0 green:64/255.0 blue:128/255.0 alpha:1] offset:CGSizeMake(0,5) radius:10];
+    [self.navigationController.navigationBar setLayerShadow:[UIColor colorWithRed:0/255.0 green:64/255.0 blue:128/255.0 alpha:0.04] offset:CGSizeMake(0,5) radius:10];
     
     self.navigationItem.titleView = self.navTitleLabel;
 }

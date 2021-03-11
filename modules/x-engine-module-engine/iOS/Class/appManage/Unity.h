cb095c9f8370b0a6047db66b7d33d3d07d65d146
liuxiao00256 <m18291165921@163.com>
2021-03-11 15:25:26 +0800

    fix(app-c):解决商城调用原生相册选择图片后，没有回调给webview的问题

--- modules/x-engine-module-engine/iOS/Class/appManage/Unity.h
+++ modules/x-engine-module-engine/iOS/Class/appManage/Unity.h
@@ -6,16 +6,17 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-
+@class WKWebView;
 @interface Unity : NSObject
 
-
+@property (nonatomic, weak) WKWebView *XXXWeb;
 //+ (NSString *)getAppKey:(NSString *)appSecret MicroApp:(NSString *)microapp;
 + (Unity *)sharedInstance;
 - (UIView *)topView;
 //获取当前屏幕显示的viewcontroller
 - (UIViewController *)getCurrentVC;
 
+;
 @end
 
 

afb77428fa4ae79fa476273280719d30e47c426e
zk <liuzq7@gmail.com>
2021-03-24 17:34:34 +0800

    chore: 增加生成代码覆盖率，应该要100%

--- modules/x-engine-module-core/iOS/Class/iOS_Modules/core/XEngineContext.m
+++ modules/x-engine-module-core/iOS/Class/iOS_Modules/core/XEngineContext.m
@@ -131,11 +131,6 @@
     return modules;
 }
 
-bool startsWith(const char *pre, const char *str) {
-    size_t lenpre = strlen(pre),
-           lenstr = strlen(str);
-    return lenstr < lenpre ? false : memcmp(pre, str, lenpre) == 0;
-}
 
 - (void)registerModuleByClass:(Class)clazz {
     if([self.moduleClasses containsObject:clazz]){

e07a53453c0deb9bc79db7d108b72b85fd3921f8
zk <liuzq7@gmail.com>
2021-03-30 16:55:10 +0800

    feat: add protocol->http:   scheme:http

---
+++ modules/x-engine-module-core/iOS/Class/iOS_Modules/native/direct/http/HTTPDirectModule.h
@@ -0,0 +1,17 @@
+//
+//  HTTPDirectModule.h
+//  ModuleApp
+//
+//  Created by zk on 2021/3/23.
+//  Copyright © 2021 zkty-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NativeModule.h"
+#import "iDirect.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HTTPDirectModule : NativeModule <iDirect>
+ @end
+
+NS_ASSUME_NONNULL_END

8c1fa96347782014349d7d22842c632c1c7de771
zk <liuzq7@gmail.com>
2021-03-30 20:06:49 +0800

    feat: 增加  x-engine-native-template

---
+++ modules/x-engine-native-template/iOS/Class/JSI_xxxx.h
@@ -0,0 +1,19 @@
+//
+//  JSI_xxxx.h
+//  xxxx
+//
+//  Created by zk on 2020/9/7.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <micros.h>
+#import "xengine_jsi_xxxx.h"
+
+NS_ASSUME_NONNULL_BEGIN
+@class SheetDTO;
+@interface JSI_xxxx : xengine_jsi_xxxx
+ 
+ @end
+
+NS_ASSUME_NONNULL_END
---
+++ modules/x-engine-native-template/iOS/Class/JSI_xxxx.m
@@ -0,0 +1,39 @@
+//
+//  JSI_xxxx.m
+//  xxxx
+//
+//  Created by zk on 2020/9/7.
+//  Copyright © 2020 edz. All rights reserved.
+
+
+#import "JSI_xxxx.h"
+#import <x-engine-module-core/JSIContext.h>
+
+@interface JSI_xxxx()
+{
+    NSTimer * timer ;
+    ContinousDTO* adto;
+    void(^hanlder)(id value,BOOL isComplete);
+    int value;
+    NSString* event;
+
+}
+@end
+
+@implementation JSI_xxxx
+ 
+  
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+@end
+ 

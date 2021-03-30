b40dfc5933c5c660a7e96f39cc39e869a18847b1
zk <liuzq7@gmail.com>
2021-03-31 03:53:57 +0800

    feat: add broad native implementation

---
+++ modules/x-engine-native-broadcast/android/app/src/main/java/com/zkty/engine/module/broadcast/broadcastApplication.java
@@ -0,0 +1,11 @@
+package com.zkty.engine.module.broadcast;
+
+import com.zkty.modules.engine.XEngineApplication;
+
+public class broadcastApplication extends XEngineApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ modules/x-engine-native-broadcast/iOS/Class/Native_broadcast.m
@@ -0,0 +1,40 @@
+//
+//  Native_broadcast.m
+//  broadcast
+//
+//  Created by zk on 2020/9/7.
+//  Copyright © 2020 edz. All rights reserved.
+
+
+#import "Native_broadcast.h"
+#import "NativeContext.h"
+#import "GlobalState.h"
+#import "XEOneWebViewPool.h"
+#import "NativeContext.h"
+@interface Native_broadcast()
+{ }
+@end
+
+@implementation Native_broadcast
+NATIVE_MODULE(Native_broadcast)
+
+ - (NSString*) moduleId{
+    return @"com.zkty.native.broadcast";
+}
+
+- (int) order{
+    return 0;
+}
+
+- (void)afterAllNativeModuleInited{
+}
+
+- (void)broadcast:(NSString*) payload{
+     for (XEOneWebViewPoolModel* wv in [XEOneWebViewPool sharedInstance].webCacheAry){
+        [wv.webView callHandler:@"com.zkty.module.engine.broadcast" arguments:payload completionHandler:^(id  _Nullable value) {
+            NSLog(@"js return value %@",value);
+        }];
+    }
+}
+@end
+ 

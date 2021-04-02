6490d132be93416044de54132d6578d60f316fcd
zk <liuzq7@gmail.com>
2021-04-03 00:13:58 +0800

    feat: 简化native 模板

--- modules/x-engine-native-template/iOS/xxxx/AppDelegate.m
+++ modules/x-engine-native-template/iOS/xxxx/AppDelegate.m
@@ -15,12 +15,15 @@
  
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    
     [[NativeContext sharedInstance] start];
 
     EntryViewController *homePageVC = [[EntryViewController alloc] init];
        UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:homePageVC];
      self.window.rootViewController = nav;
-    
+    [self.window makeKeyAndVisible];
+
     return YES;
 }
   

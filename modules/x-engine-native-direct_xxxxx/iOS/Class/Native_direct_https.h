f25c5632b9eb6d1bbdbd87682713e8ef8391602f
zk <liuzq7@gmail.com>
2021-04-06 17:59:40 +0800

    chore: temp add xxxxx jsi

---
+++ modules/x-engine-native-direct_xxxxx/iOS/Class/Native_direct_https.h
@@ -0,0 +1,17 @@
+//
+//  Native_direct_https.h
+//  direct_https
+//
+//  Copyright © 2020 @zkty-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NativeModule.h"
+#import "iDirect.h"
+
+NS_ASSUME_NONNULL_BEGIN
+@interface Native_direct_https : NativeModule <iDirect>
+ 
+@end
+
+NS_ASSUME_NONNULL_END

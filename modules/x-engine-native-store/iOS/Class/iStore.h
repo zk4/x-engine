5e7270fcf09826400f755a723ab5a734d61fa054
zk <liuzq7@gmail.com>
2021-04-08 00:08:36 +0800

    chore: update tests

--- modules/x-engine-native-store/iOS/Class/iStore.h
+++ modules/x-engine-native-store/iOS/Class/iStore.h
@@ -10,9 +10,25 @@
 #define istore_h
 
 @protocol iStore <NSObject>
+
+/// 读取相应的键
+/// @param key 键 参看： set
 - (id)get:(NSString *)key;
+
+/// 存储键值对
+/// @param key 键: 如果有 namespace 的需求，应该在顶层再自行编码。如
+/// namespace1:key2  namespace2:key2 作为 key 值。
+/// @param val 任意 Object 类型
 - (void)set:(NSString *)key val:(id)val;
+
+/// api 存档
+/// 默认在 UIApplicationDidEnterBackgroundNotification 时，会进行持久化
 - (void)saveTodisk;
-- (void)readFromDisk:(BOOL)merge;
+
+/// api 取档
+/// 默认在 UIApplicationDidFinishLaunchingNotification 时，
+/// 会自动读取档案。
+/// @param merge 是否要 merge 已经存在的档案，当前档案将被覆盖
+- (void)loadFromDisk:(BOOL)merge;
 @end
 #endif /* istore_h */
--- modules/x-engine-native-store/iOS/iOSTests/iOSTests.m
+++ modules/x-engine-native-store/iOS/iOSTests/iOSTests.m
@@ -5,7 +5,7 @@
 
 #import <XCTest/XCTest.h>
 #import "Native_store.h"
-@interface iOSTests : XCTestCase
+ @interface iOSTests : XCTestCase
 @property(strong,nonatomic) id<iStore> store;
 @end
 
@@ -13,6 +13,7 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+
     self.store = [Native_store new];
 }
 
@@ -41,5 +42,32 @@
    id a= [_store get:@"Hello"];
     XCTAssertEqualObjects(array,a, @"should equal");
 }
+
  
+
+- (void)testLoadFromDiskMerge {
+    [_store set:@"oldkey" val:@"123"];
+    [_store saveTodisk];
+    [_store set:@"newkey" val:@"abc"];
+    [_store loadFromDisk:TRUE];
+    NSString* newVal = [_store get:@"newkey"];
+    NSString* oldVal = [_store get:@"oldkey"];
+    XCTAssertEqualObjects(@"abc",newVal, @"should equal");
+    XCTAssertEqualObjects(@"123",oldVal, @"should equal");
+
+}
+
+
+
+- (void)testLoadFromDiskDonMerge {
+   [_store set:@"oldkey" val:@"123"];
+   [_store saveTodisk];
+   [_store set:@"newkey" val:@"abc"];
+   [_store loadFromDisk:FALSE];
+   NSString* newVal = [_store get:@"newkey"];
+   NSString* oldVal = [_store get:@"oldkey"];
+   XCTAssertEqualObjects(nil,newVal, @"should equal");
+   XCTAssertEqualObjects(@"123",oldVal, @"should equal");
+
+}
 @end

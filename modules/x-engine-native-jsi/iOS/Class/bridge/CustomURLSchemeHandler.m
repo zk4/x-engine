67e4327b31e483c7396fb6b761e1dc54fc3bf41b
zk <liuzq7@gmail.com>
2021-04-12 20:36:41 +0800

    chore: 回滚网络拦截,会崩溃
    chore: 回滚网络拦截,会崩溃

--- modules/x-engine-native-jsi/iOS/Class/bridge/CustomURLSchemeHandler.m
+++ modules/x-engine-native-jsi/iOS/Class/bridge/CustomURLSchemeHandler.m
@@ -1,19 +1,27 @@
+//
+//  CustomURLSchemeHandler.m
+//  testWebview
+//
+//  Created by 杨方阔 on 2019/12/9.
+//  Copyright © 2019 Mike. All rights reserved.
+//
+
 #import "CustomURLSchemeHandler.h"
 #import <WebKit/WebKit.h>
 #import "WebViewFactory.h"
-#import "NativeContext.h"
 #import "iSecurify.h"
-
 static NSString const*SchemeKey = @"SchemeKey";
 static NSString const*TaskKey = @"TaskKey";
 API_AVAILABLE(ios(11.0))
 @interface CustomURLSchemeHandler ()
 @property (nonatomic, strong) NSMutableDictionary<NSURL *, NSDictionary<NSString *, id> *> *dic;
+@property (nonatomic, strong) id<iSecurify>  security;
 @end
 
 @implementation CustomURLSchemeHandler
 
-- (instancetype)init {
+- (instancetype)init
+{
     self = [super init];
     if (self) {
         self.dic = [@{} mutableCopy];
@@ -21,44 +29,43 @@ API_AVAILABLE(ios(11.0))
     return self;
 }
 - (void)webView:(WKWebView *)webView startURLSchemeTask:(id <WKURLSchemeTask>)urlSchemeTask API_AVAILABLE(ios(11.0)){
-    
-    // 判断有没有白名单
-    id<iSecurify> securify = [[NativeContext sharedInstance] getModuleByProtocol:@protocol(iSecurify)];
-    if(securify){
-        BOOL isAvailable = [securify judgeNetworkIsAvailableWithHostName:urlSchemeTask.request.URL.host];
-        if (!isAvailable) {
-            NSString *msg = [NSString stringWithFormat:@"%@不在白名单内", urlSchemeTask.request.URL.host];
-            [self promptWithMessage:msg];
-            return;
-        }
-    }
-
+    NSURLRequest *request = urlSchemeTask.request;
+    /// TODO: 网络拦截位置
+    BOOL security_on = YES;
+    if(!security_on){
         __weak typeof(self)weakSelf = self;
-    NSURLSessionDataTask * task = [[NSURLSession sharedSession] dataTaskWithRequest:urlSchemeTask.request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        __strong CustomURLSchemeHandler *strongSelf = weakSelf;
-        if(strongSelf){
-            id <WKURLSchemeTask> delegate;
-            NSDictionary *dic;
-            @synchronized (self) {
-                dic = strongSelf.dic[urlSchemeTask.request.URL];
+        NSURLSessionDataTask * task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            __strong CustomURLSchemeHandler *strongSelf = weakSelf;
+            if(strongSelf){
+                id <WKURLSchemeTask> delegate;
+                NSDictionary *dic;
+                @synchronized (self) {
+                    dic = strongSelf.dic[request.URL];
+                }
+                delegate = dic[SchemeKey];
+                if(error){
+                    [delegate didFailWithError:error];
+                }else{
+                    [delegate didReceiveResponse:response];
+                    [delegate didReceiveData:data];
+                    [delegate didFinish];
+                }
+                strongSelf.dic[request.URL] = nil;
             }
-            delegate = dic[SchemeKey];
-            if(error){
-                [delegate didFailWithError:error];
-            }else{
-                [delegate didReceiveResponse:response];
-                [delegate didReceiveData:data];
-                [delegate didFinish];
-            }
-            strongSelf.dic[urlSchemeTask.request] = nil;
-        }
-    }];
-    self.dic[urlSchemeTask.request.URL] = @{
-        SchemeKey:urlSchemeTask,
-        TaskKey:task,
-    };
-    [task resume];
-
+        }];
+        self.dic[request.URL] = @{
+            SchemeKey:urlSchemeTask,
+            TaskKey:task,
+        };
+        [task resume];
+    }else{
+        UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"" message:[NSString stringWithFormat:@"%@不在白名单内",request.URL.host] preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+        [errorAlert addAction:sureAction];
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:errorAlert animated:YES completion:nil];
+        [urlSchemeTask didFailWithError:[[NSError alloc] init]];
+    }
+ 
 }
 
 - (void)webView:(WKWebView *)webVie stopURLSchemeTask:(nonnull id<WKURLSchemeTask>)urlSchemeTask API_AVAILABLE(ios(11.0)) {
@@ -68,17 +75,6 @@ API_AVAILABLE(ios(11.0))
         NSURLSessionDataTask *task = dic[TaskKey];
         [task cancel];
     }
-}
-
-/// 提示
-/// @param message 提示内容
-- (void)promptWithMessage:(NSString *)message  {
-    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"" message:message preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        [[UIApplication sharedApplication].keyWindow.rootViewController.navigationController popViewControllerAnimated:YES];
-    }];
-    [errorAlert addAction:sureAction];
-    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:errorAlert animated:YES completion:^{}];
 }
 
 @end

78e35211acc46947248ef6ad71adbc2775bc356e
zk <liuzq7@gmail.com>
2021-04-13 22:32:27 +0800

    fix: 引擎 jsi api args bug

--- modules/x-engine-module-engine/h5/src/index.js
+++ modules/x-engine-module-engine/h5/src/index.js
@@ -1,142 +1,156 @@
 import dsbridge from "./dsbridge";
 const module_names = new Set([]);
-const patch = {}
+const patch = {};
 
 function isFunction(functionToCheck) {
- return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+  return (
+    functionToCheck && {}.toString.call(functionToCheck) === "[object Function]"
+  );
 }
 
 function isObject(val) {
-    if (val === null) { return false;}
-    return ( (typeof val === 'function') || (typeof val === 'object') );
+  if (val === null) {
+    return false;
+  }
+  return typeof val === "function" || typeof val === "object";
 }
 
 function isString(x) {
-  return Object.prototype.toString.call(x) === "[object String]"
+  return Object.prototype.toString.call(x) === "[object String]";
 }
 
-function isHybrid(){
-  return window && window._dswk===true;
+function isHybrid() {
+  return window && window._dswk === true;
 }
 let xengine = {
-  patch   : patch,
+  patch: patch,
   platform: platform(),
-  hybrid  : true,
-  isHybrid  : isHybrid,
-  bridge  : dsbridge,
-  use     : use,
-  api     : api,
+  hybrid: true,
+  isHybrid: isHybrid,
+  bridge: dsbridge,
+  use: use,
+  api: api,
   broadcastOn: broadcastOn,
   broadcastOff: broadcastOff,
-  assert : xassert
+  assert: xassert,
 };
 
-function xassert(targetID,expression){
-    if(expression){
-      document.getElementById(targetID).style.backgroundColor = 'green';
-    }else{
-      document.getElementById(targetID).style.backgroundColor = 'red';
-    }
-}
-function api(jsimoduleId,funcname,args,cb){
-  if (args.hasOwnProperty('__event__')){
-      only_idx++;
-     let eventcb = args['__event__'];
-     if(!isFunction(eventcb)) throw('__event__ 必须为函数');
-     args['__event__']  = ns+"."+funcname+'.__event__'+only_idx;
-      xengine.bridge.register(args['__event__'], (res) => {
-          return eventcb(res);
-      })
+function xassert(targetID, expression) {
+  if (expression) {
+    document.getElementById(targetID).style.backgroundColor = "green";
+  } else {
+    document.getElementById(targetID).style.backgroundColor = "red";
   }
-  return dsbridge.call(jsimoduleId+"."+funcname,args,cb)
 }
-function broadcastOff(){
-    xengine.bridge.unregister("com.zkty.module.engine.broadcast");
-}
-
-function broadcastOn(eventcb){
-  //use("com.zkty.module.engine","broadcast")
-    xengine.bridge.register("com.zkty.module.engine.broadcast", (res) => {
+function api(jsimoduleId, funcname, args, cb) {
+  if (args.hasOwnProperty("__event__")) {
+    only_idx++;
+    if (args) {
+      let eventcb = args["__event__"];
+      if (!isFunction(eventcb)) throw "__event__ 必须为函数";
+      args["__event__"] = ns + "." + funcname + ".__event__" + only_idx;
+      xengine.bridge.register(args["__event__"], (res) => {
+        // 处理__event__ 回调
         return eventcb(res);
-    })
-}
-let only_idx=0;
-function use(ns,funcs){
-    if(module_names.has(ns)){
-      throw(ns+ ',注册无效,模块已存在,xengine.use("'+ns+'") 只允许调用一次;')
+      });
     }
-    module_names.add(ns)
-    console.log(ns+ ',js 注册成功')
+  }
+  // 处理 sync, async 方法
+  // aysnc 会通过 cb 传递
+  // sync 通过 return 返回
+  return dsbridge.call(jsimoduleId + "." + funcname, args, cb);
 
-    let _call = function(funcname,args){
-        if (args.hasOwnProperty('__event__')){
-            only_idx++;
-           let eventcb = args['__event__'];
-           if(!isFunction(eventcb)) throw('__event__ 必须为函数');
-           args['__event__']  = ns+"."+funcname+'.__event__'+only_idx;
-            xengine.bridge.register(args['__event__'], (res) => {
-                return eventcb(res);
-            })
-        }
-        
-        if(funcname.startsWith('sync')){
-          return xengine.bridge.call(ns+"."+funcname,args);
-        }
-        else{
-          let p =new Promise((resolve,reject)=>{
-            const warning_msg = "x-engine 0.1.0 将不再支持 promise,改用参数里的　__ret__做为异步返回值,以支持多次返回.或者直接调用函数同步返回";
-            console.warn(warning_msg);
-            xengine.bridge.call(ns+"."+funcname, args, function (res) {
-              // only resolve once
-              resolve(res);
-              if(args['__ret__']){
-                return args['__ret__'](res)
-              }
-            })
-          });
-          return p;
-        }
-    };
+}
+function broadcastOff() {
+  xengine.bridge.unregister("com.zkty.module.engine.broadcast");
+}
 
-    return funcs.reduce((acc,cur,i)=>{
-      if(isObject(cur)){
-        acc[cur.name] = args => _call(cur.name,{...cur.default_args,...args});
-      }else if(isString(cur)){
-        acc[cur] = args => _call(cur,args);
-      }else{
-        throw("仅支持 string 与 {name:xxx, default_args:{...}}")
-      }
-      return acc;
-    },{})
+function broadcastOn(eventcb) {
+  //use("com.zkty.module.engine","broadcast")
+  xengine.bridge.register("com.zkty.module.engine.broadcast", (res) => {
+    return eventcb(res);
+  });
+}
+let only_idx = 0;
+function use(ns, funcs) {
+  if (module_names.has(ns)) {
+    throw ns + ',注册无效,模块已存在,xengine.use("' + ns + '") 只允许调用一次;';
+  }
+  module_names.add(ns);
+  console.log(ns + ",js 注册成功");
+
+  let _call = function (funcname, args) {
+    if (args.hasOwnProperty("__event__")) {
+      only_idx++;
+      let eventcb = args["__event__"];
+      if (!isFunction(eventcb)) throw "__event__ 必须为函数";
+      args["__event__"] = ns + "." + funcname + ".__event__" + only_idx;
+      xengine.bridge.register(args["__event__"], (res) => {
+        return eventcb(res);
+      });
+    }
+
+    if (funcname.startsWith("sync")) {
+      return xengine.bridge.call(ns + "." + funcname, args);
+    } else {
+      let p = new Promise((resolve, reject) => {
+        const warning_msg =
+          "x-engine 0.1.0 将不再支持 promise,改用参数里的　__ret__做为异步返回值,以支持多次返回.或者直接调用函数同步返回";
+        console.warn(warning_msg);
+        xengine.bridge.call(ns + "." + funcname, args, function (res) {
+          // only resolve once
+          resolve(res);
+          if (args["__ret__"]) {
+            return args["__ret__"](res);
+          }
+        });
+      });
+      return p;
+    }
+  };
+
+  return funcs.reduce((acc, cur, i) => {
+    if (isObject(cur)) {
+      acc[cur.name] = (args) =>
+        _call(cur.name, { ...cur.default_args, ...args });
+    } else if (isString(cur)) {
+      acc[cur] = (args) => _call(cur, args);
+    } else {
+      throw "仅支持 string 与 {name:xxx, default_args:{...}}";
+    }
+    return acc;
+  }, {});
 }
 
 Object.defineProperty(xengine, "bridge", {
-  get(){
+  get() {
     return dsbridge;
   },
   set: function () {
-    throw('dsbridge不能被修改')
-  }
+    throw "dsbridge不能被修改";
+  },
 });
 
 function platform() {
-     var ua = navigator.userAgent,
-     isWindowsPhone = /(?:Windows Phone)/.test(ua),
-     isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone, 
-     isAndroid = /(?:Android)/.test(ua), 
-     isFireFox = /(?:Firefox)/.test(ua), 
-     isChrome = /(?:Chrome|CriOS)/.test(ua),
-     isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
-     isPhone = /(?:iPhone)/.test(ua) && !isTablet,
-     isPc = !isPhone && !isAndroid && !isSymbian;
-     return {
-          isTablet: isTablet,
-          isPhone: isPhone,
-          isAndroid : isAndroid,
-          isPc : isPc
-     };
-};
-
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTablet =
+      /(?:iPad|PlayBook)/.test(ua) ||
+      (isAndroid && !/(?:Mobile)/.test(ua)) ||
+      (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+  return {
+    isTablet: isTablet,
+    isPhone: isPhone,
+    isAndroid: isAndroid,
+    isPc: isPc,
+  };
+}
 
 // 监听输入框的软键盘弹起和收起事件
 function listenKeybord($input) {
@@ -188,12 +202,12 @@ var $inputs = document.querySelectorAll(".input");
 for (var i = 0; i < $inputs.length; i++) {
   listenKeybord($inputs[i]);
 }
- 
-patch.disableDoubleTapScroll=function(ms){
+
+patch.disableDoubleTapScroll = function (ms) {
   ms = ms || 500;
-  console.log("禁用双击滑动,两次点击冷却时间为"+ms+" ms")
-   //禁止双击时, webview 自动上移
-   //不是个好方案, 会导致快速点击按钮失效
+  console.log("禁用双击滑动,两次点击冷却时间为" + ms + " ms");
+  //禁止双击时, webview 自动上移
+  //不是个好方案, 会导致快速点击按钮失效
   var agent = navigator.userAgent.toLowerCase();
   var iLastTouch = null;
   if (agent.indexOf("iphone") >= 0 || agent.indexOf("ipad") >= 0) {
@@ -212,5 +226,6 @@ patch.disableDoubleTapScroll=function(ms){
       false
     );
   }
-}
+};
 export default xengine;
+

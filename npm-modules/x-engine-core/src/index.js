e5ffdfef6b66b7313027e677db6054556a19271b
Cwz-github <littlegrass003@gmail.com>
2021-04-15 15:36:57 +0800

    fix: no ns in core/api

--- npm-modules/x-engine-core/src/index.js
+++ npm-modules/x-engine-core/src/index.js
@@ -42,13 +42,14 @@ function xassert(targetID, expression) {
     document.getElementById(targetID).style.backgroundColor = "red";
   }
 }
+let only_idx = 0;
 function api(jsimoduleId, funcname, args, cb) {
   if (args) {
     if (args.hasOwnProperty("__event__")) {
       only_idx++;
       let eventcb = args["__event__"];
       if (!isFunction(eventcb)) throw "__event__ 必须为函数";
-      args["__event__"] = ns + "." + funcname + ".__event__" + only_idx;
+      args["__event__"] = jsimoduleId + "." + funcname + ".__event__" + only_idx;
       xengine.bridge.register(args["__event__"], (res) => {
         // 处理__event__ 回调
         return eventcb(res);
@@ -70,7 +71,6 @@ function broadcastOn(eventcb) {
     return eventcb(res);
   });
 }
-let only_idx = 0;
 function use(ns, funcs) {
   if (module_names.has(ns)) {
     throw ns + ',注册无效,模块已存在,xengine.use("' + ns + '") 只允许调用一次;';

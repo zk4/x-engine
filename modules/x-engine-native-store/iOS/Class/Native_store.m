cb6bebba93ad3ac90793356cdca6d22ec524989d
Cwz-github <littlegrass003@gmail.com>
2021-04-22 00:28:35 +0800

    feat: store中添加appid

--- modules/x-engine-native-store/iOS/Class/Native_store.m
+++ modules/x-engine-native-store/iOS/Class/Native_store.m
@@ -9,6 +9,7 @@
 #import "Native_store.h"
 #import <UIKit/UIKit.h>
 #import <micros.h>
+#import "GlobalState.h"
 #define X_ENGINE_STORE_KEY @"@@x-engine-store"
 
 @interface Native_store ()
@@ -25,6 +26,9 @@ NATIVE_MODULE(Native_store)
 - (int)order {
     return 0;
 }
+
+- (void)afterAllNativeModuleInited {}
+
 - (instancetype)init {
     self = [super init];
     if (self) {
@@ -54,33 +58,47 @@ NATIVE_MODULE(Native_store)
     return self;
 }
 
-- (void)afterAllNativeModuleInited {
-}
+
+// 疑问1
+// 怎么判断ios调用还是js调用 通过host有没有值？
+// 目前想到的解决方案是的在增加3个方法让原生调用?
+// - (id)nativeGet
+// - (id)nativeSet
+// - (id)nativeDel
+
+// 疑问2
+// 下面是拿到host值 拼接的key
+// 依赖了x-engine-native-jsi模块 这样是否合适 
 
 - (id)get:(NSString *)key {
-    return [_store objectForKey:key];
+    NSString *customkey = [NSString stringWithFormat:@"%@-%@", [[GlobalState sharedInstance] getLastHost], key];
+    NSLog(@"%@", customkey);
+    return [_store objectForKey:customkey];
 }
 
 - (void)set:(NSString *)key val:(id)val {
-    [_store setObject:val forKey:key];
+    NSString *customkey = [NSString stringWithFormat:@"%@-%@", [[GlobalState sharedInstance] getLastHost], key];
+    NSLog(@"%@", customkey);
+    [_store setObject:val forKey:customkey];
 }
 
-- (void) del:(NSString*)key{
-     [_store removeObjectForKey:key];
+- (void)del:(NSString*)key{
+    NSString *customkey = [NSString stringWithFormat:@"%@-%@", [[GlobalState sharedInstance] getLastHost], key];
+    NSLog(@"%@", customkey);
+     [_store removeObjectForKey:customkey];
 }
 
-- (void) saveTodisk{
-    [[NSUserDefaults standardUserDefaults] setObject:self.store   forKey:X_ENGINE_STORE_KEY];
+- (void)saveTodisk{
+    [[NSUserDefaults standardUserDefaults] setObject:self.store forKey:X_ENGINE_STORE_KEY];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
-- (void) loadFromDisk:(BOOL)merge{
+
+- (void)loadFromDisk:(BOOL)merge {
     if(!merge){
         [self.store removeAllObjects];
     }
     [self.store addEntriesFromDictionary:[[NSUserDefaults standardUserDefaults] objectForKey:X_ENGINE_STORE_KEY]];
 }
-
-
 @end
 
 #undef X_ENGINE_STORE_KEY

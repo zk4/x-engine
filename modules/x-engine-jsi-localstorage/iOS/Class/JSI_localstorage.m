99f810a9cc731352c6db26fe4992922c8a5b749f
zk <liuzq7@gmail.com>
2021-04-22 12:25:50 +0800

    chore(localstorage): add assert

--- modules/x-engine-jsi-localstorage/iOS/Class/JSI_localstorage.m
+++ modules/x-engine-jsi-localstorage/iOS/Class/JSI_localstorage.m
@@ -25,23 +25,22 @@ JSI_MODULE(JSI_localstorage)
     _store = XENP(iStore);
 }
 
-
 /// 生成 microapp 唯一的 key,现在这个实现对于 omp 来说是有 bug 的. 如果两个微应用来自同一个 host,则会冲突.
 /// TODO: 使用真实的 microapp id
 /// @param key key
 - (NSString *) genkey:(NSString*) key{
+    assert([[GlobalState sharedInstance] getLastHost]!=nil);
+    assert(key!=nil);
     return  [NSString stringWithFormat:@"%@:%@", [[GlobalState sharedInstance] getLastHost], key];
 }
+
 - (NSString *)_get:(NSString *)dto {
     return [_store get:[self genkey:dto]];
-
 }
- 
 
 - (void)_set:(_0_com_zkty_jsi_localstorage_DTO *)dto {
     [_store set:[self genkey:dto.key] val:dto.val];
-
 }
 
- 
+
 @end

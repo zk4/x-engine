776fa467860d8a34d2da8b6212770de36728767c
Cwz-github <littlegrass003@gmail.com>
2021-04-22 18:01:15 +0800

    feat: 修改x-engine-router判断平台代码, 兼容安卓认不出的问题

--- microApps/H5-Sample/src/App.vue
+++ microApps/H5-Sample/src/App.vue
@@ -40,15 +40,4 @@ export default {
 .bounce-leave-active {
   animation: bounce-in 0.5s reverse;
 }
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.1);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
 </style>
--- npm-modules/x-engine-router/src/index.js
+++ npm-modules/x-engine-router/src/index.js
@@ -3,7 +3,7 @@ import XEngine from '@zkty-team/x-engine-core'
 function intercept(VueRouter, scheme) {
     const originalRouterPush = VueRouter.prototype.push;
     VueRouter.prototype.push = function push(location) {
-        if (XEngine.platform.isAndroid || xengine.platform.isPhone) {
+        if (XEngine.isHybrid()) {
             XEngine.api('com.zkty.jsi.direct', 'push', {
                 scheme: scheme,
                 pathname: '',
@@ -16,13 +16,28 @@ function intercept(VueRouter, scheme) {
                 console.log('res :>> ', res);
             })
         } else {
-            return originalRouterPush.call(this, location);
+            //  为了适配android
+            if (XEngine.platform.isAndroid) {
+                XEngine.api('com.zkty.jsi.direct', 'push', {
+                    scheme: scheme,
+                    pathname: '',
+                    fragment: location.path,
+                    query: location.query,
+                    params: {
+                        hideNavbar: true
+                    }
+                }, function (res) {
+                    console.log('res :>> ', res);
+                })
+            } else {
+                return originalRouterPush.call(this, location);
+            }
         }
     }
 
     const originalRouterGo = VueRouter.prototype.go;
     VueRouter.prototype.go = function go(location) {
-        if (XEngine.platform.isAndroid || xengine.platform.isPhone) {
+        if (XEngine.isHybrid()) {
             XEngine.api('com.zkty.jsi.direct', 'back', {
                 scheme: scheme,
                 pathname: '',
@@ -34,7 +49,21 @@ function intercept(VueRouter, scheme) {
                 console.log('res :>> ', res);
             })
         } else {
-            return originalRouterGo.call(this, location);
+            //  为了适配android
+            if (XEngine.platform.isAndroid) {
+                XEngine.api('com.zkty.jsi.direct', 'back', {
+                    scheme: scheme,
+                    pathname: '',
+                    fragment: location + "",
+                    params: {
+                        hideNavbar: true
+                    }
+                }, function (res) {
+                    console.log('res :>> ', res);
+                })
+            } else {
+                return originalRouterGo.call(this, location);
+            }
         }
     }
 }

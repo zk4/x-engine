2454e18b9631b34901dd896b5d1e8d515bac7913
Mr. Zhao <localhost3585@gmail.com>
2021-04-23 20:09:55 +0800

    fix: scret改名

--- modules/x-engine-jsi-secret/iOS/Class/JSI_secret.h
+++ modules/x-engine-jsi-secret/iOS/Class/JSI_secret.h
@@ -1,17 +1,17 @@
 //
-//  JSI_secrect.h
-//  secrect
+//  JSI_secret.h
+//  secret
 //
 //  Created by zk on 2020/9/7.
 //  Copyright © 2020 edz. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "xengine_jsi_secrect.h"
+#import "xengine_jsi_secret.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface JSI_secrect : xengine_jsi_secrect
+@interface JSI_secret : xengine_jsi_secret
 @end
 
 NS_ASSUME_NONNULL_END

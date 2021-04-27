1e44b7a44ca2111d260118d405666071e2539500
zk <liuzq7@gmail.com>
2021-04-27 14:47:07 +0800

    feat: add vuex jsi

---
+++ modules/x-engine-jsi-vuex/iOS/Class/JSI_vuex.h
@@ -0,0 +1,17 @@
+//
+//  JSI_vuex.h
+//  vuex
+//
+//  Created by zk on 2020/9/7.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "xengine_jsi_vuex.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSI_vuex : xengine_jsi_vuex
+@end
+
+NS_ASSUME_NONNULL_END

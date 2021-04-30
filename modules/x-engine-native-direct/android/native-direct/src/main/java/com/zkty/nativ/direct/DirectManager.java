60f68335107281a3be4dadc994e937c0c9d96e22
xiefei <xiefei_525@163.com>
2021-04-30 18:06:32 +0800

    fix: 修改direct push

--- modules/x-engine-native-direct/android/native-direct/src/main/java/com/zkty/nativ/direct/DirectManager.java
+++ modules/x-engine-native-direct/android/native-direct/src/main/java/com/zkty/nativ/direct/DirectManager.java
@@ -23,20 +23,13 @@ public class DirectManager {
     }
 
 
-    public static void push(String url) {
+    public static void push(String url, Map<String, String> params) {
 
         try {
             URL url1 = URL.parse(url);
             String scheme = url1.getScheme();
-            String protocol = "https:";
-            if ("omp".equals(scheme)) protocol = "http:";
-            if ("http".equals(scheme)) protocol = "http:";
-            if ("https".equals(scheme)) protocol = "https:";
-            if ("microapp".equals(scheme)) protocol = "file:";
-
             //URL解析的 fragment 包含query，eg：/mall2/orderlist?selectedIndex=0，故query传null
-            XEngineWebActivityManager.sharedInstance().startXEngineActivity(XEngineApplication.getCurrentActivity(), protocol, url1.getHost(), url1.getPath(), url1.getFragment(), null, true);
-
+            push(scheme, url1.getHost(), url1.getPath(), url1.getFragment(), null, params);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }

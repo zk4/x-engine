be5432bbc382e0b9e09d761865137bd1123193ce
Cwz-github <littlegrass003@gmail.com>
2021-05-20 14:57:22 +0800

    feat: x-engine-lifecycle更新至1.0.3

--- npm-modules/x-engine-lifecycle/src/lifeCycle.js
+++ npm-modules/x-engine-lifecycle/src/lifeCycle.js
@@ -7,38 +7,34 @@
  * @FilePath: /x-engine/microApps/H5-Sample/src/lifeCycle.js
  */
 import xengine from "@zkty-team/x-engine-core";
-let Vue;
 
+const ON_NATIVE_SHOW = "onNativeShow"; // 原生显示
+const ON_WEBVIEW_SHOW = "onWebviewShow"; // 原生显示
+const ON_NATIVE_HIDE = "onNativeHide"; // 原生隐藏
+const ON_NATIVE_DESTROYED = "onNativeDestroyed"; // 原生销毁
+
+let Vue;
 export function install(_Vue) {
   if (Vue !== _Vue) {
     Vue = _Vue;
-    Vue.$$x_engine_broadcast_once = false;
   }
   Vue.mixin({
-    created() {
-      let that = this;
-      xengine.broadcastOn((type, payload) => {
-        // $$x_engine_broadcast_once 用来解决回退上一个webview出发俩次vue 生命周期
-        if (type === "@@VUE_LIFECYCLE_EVENT") {
-          if (that.$$x_engine_broadcast_once) {
-            let hook = "mounted";
-            const handlers = that.$options[hook];
-            if (handlers) {
-              for (let i = 0, j = handlers.length; i < j; i++) {
-                handlers[i].call(that);
-              }
-            }
-            if (that._hasHookEvent) {
-              that.$emit("hook:" + hook);
-            }
-          } else {
-            that.$$x_engine_broadcast_once = true;
-          }
+    beforeCreate() {
+      xengine.onLifecycle((type, payload) => {
+        if (type == ON_NATIVE_SHOW) {
+          this.onNativeShow?.();
+        } else if (type == ON_WEBVIEW_SHOW) {
+          this.onNativeShow?.();
+        } else if (type == ON_NATIVE_HIDE) {
+          this.onNativeHide?.();
+        } else if (type == ON_NATIVE_DESTROYED) {
+          this.onNativeDestroyed?.();
         }
       });
     },
   });
 }
+
 export default {
   install,
 };

e199c7fbdc746dff62066a41cbbb18410cb00d37
wangsheng <1355118962@qq.com>
2021-05-24 20:43:25 +0800

    feat: 合并pull 下来的代码

--- modules/x-engine-app/iOS/Class/iOS_Demo/OneViewController.m
+++ modules/x-engine-app/iOS/Class/iOS_Demo/OneViewController.m
@@ -39,6 +39,6 @@
 //    [director push:@"microapp" host:@"com.gm.microapp.mine" pathname:@""  fragment:@"/" query:nil  params:@{@"hideNavbar":@TRUE}];
 
     // 远程
-    [director push:@"omp" host:@"10.2.128.61:8080" pathname:@""  fragment:@"/" query:nil  params:@{@"hideNavbar":@TRUE}];
+    [director push:@"omp" host:@"10.2.128.73:3000" pathname:@""  fragment:@"/" query:nil  params:@{@"hideNavbar":@TRUE}];
 }
 @end
--- npm-modules/x-engine-ui/vue.config.js
+++ npm-modules/x-engine-ui/vue.config.js
@@ -1,9 +1,22 @@
+/*
+ * @Author: sheng.wang
+ * @Date: 2021-04-28 14:34:33
+ * @LastEditTime: 2021-05-24 19:09:58
+ * @LastEditors: sheng.wang
+ * @Description: 
+ * @FilePath: /x-engine/npm-modules/x-engine-ui/vue.config.js
+ */
 module.exports = {
-    devServer: {
-      overlay: {
-        warnings: false, // 不显示警告
-        errors: false	   // 不显示错误
-      }
-    },
-    lintOnSave:false // 关闭eslint检查
+  devServer: {
+    overlay: {
+      warnings: false, // 不显示警告
+      errors: false	   // 不显示错误
+    }
+  },
+  lintOnSave: false, // 关闭eslint检查
+  chainWebpack: config => {
+    config.externals({
+      '@zkty-team/x-engine-core': '@zkty-team/x-engine-core'
+    });
   }
+}

4fc4b656ad8c2550868979a71ef769ffce85602e
xiefei <xiefei_525@163.com>
2021-05-25 16:24:31 +0800

    fix: 修改Optional注解路径

--- modules/x-engine-jsi-direct/android/jsi-direct/src/main/java/com/zkty/jsi/xengine_jsi_direct.java
+++ modules/x-engine-jsi-direct/android/jsi-direct/src/main/java/com/zkty/jsi/xengine_jsi_direct.java
@@ -10,10 +10,10 @@
   import android.webkit.JavascriptInterface;
   import com.alibaba.fastjson.JSON;
   import com.alibaba.fastjson.JSONObject;
+  import com.zkty.nativ.core.annotation.Optional;
   import com.zkty.nativ.jsi.bridge.CompletionHandler;
   import com.zkty.nativ.jsi.JSIModule;
   import androidx.annotation.Nullable;
-  import com.zkty.nativ.jsi.annotation.Optional;
 
   
   class DirectPushDTO {

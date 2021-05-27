36b83640945e8fd421e4dda0e15aa07c43ac91c7
Mr. Zhao <localhost3585@gmail.com>
2021-05-27 11:35:10 +0800

    fix: 增加分享模块pod文件

--- modules/x-engine-jsi-share/iOS/Class/JSI_share.m
+++ modules/x-engine-jsi-share/iOS/Class/JSI_share.m
@@ -8,7 +8,7 @@
 
 #import "JSI_share.h"
 #import "JSIContext.h"
-#import "NativeContext.h"
+#import "XENativeContext.h"
 
 @interface JSI_share()
 @end
@@ -31,38 +31,6 @@ JSI_MODULE(JSI_share)
     
 }
 
-- (NamedDTO *)_namedObject {
-    NamedDTO* ret = [NamedDTO new];
-    ret.title=@"_namedObject sync";
-    ret.titleSize=10000;
-    return ret;
-}
-
-
-- (void)_namedObject:(void (^)(NamedDTO *, BOOL))completionHandler {
-    NamedDTO* ret = [NamedDTO new];
-    ret.title=@"_namedObject async";
-    ret.titleSize=10000;
-    completionHandler(ret,TRUE);
-}
-
-
-- (_0_com_zkty_jsi_share_DTO *)_nestedAnonymousObject {
-    _0_com_zkty_jsi_share_DTO * ret =[_0_com_zkty_jsi_share_DTO new];
-    ret.a=@"hello";
-    ret.i =[_1_com_zkty_jsi_share_DTO new];
-    ret.i.n1=@"_nestedAnonymousObject sync";
-    return ret;
-}
-
-
-- (void)_nestedAnonymousObject:(void (^)(_0_com_zkty_jsi_share_DTO *, BOOL))completionHandler {
-    _0_com_zkty_jsi_share_DTO * ret =[_0_com_zkty_jsi_share_DTO new];
-    ret.a=@"hello";
-    ret.i =[_1_com_zkty_jsi_share_DTO new];
-    ret.i.n1=@"_nestedAnonymousObject async";
-    completionHandler(ret,TRUE);
-}
 
 - (NSString *)_simpleArgMethod:(NSString *)dto {
     return @"from native sync";

0a6ccd27a59b202f896cf7acaba178196f8e03a2
MAJI <18510951471@163.com>
2021-05-28 15:27:39 +0800

    wip: 修改网络拦截判断，删除module1.0

--- modules/x-engine-module-localstorage/h5/test/index_spec.js
+++
@@ -1,6 +0,0 @@
-import _debug from "../src/index.js"
-describe('xengine._debug', function(){
-  it('should exist xengine_debug', function(){
-    expect(_debug !== null);
-  });
-});
--- modules/x-engine-native-network/android/native-network/src/main/java/com/zkty/nativ/network/okhttp/OkhttpProvidede.java
+++ modules/x-engine-native-network/android/native-network/src/main/java/com/zkty/nativ/network/okhttp/OkhttpProvidede.java
@@ -1,10 +1,12 @@
 package com.zkty.nativ.network.okhttp;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.zkty.nativ.network.NetworkMaster;
 import com.zkty.nativ.network.net.HttpsUtil;
 import com.zkty.nativ.network.net.Tls12SocketFactory;
+import com.zkty.nativ.network.utils.LogUtils;
 
 import org.apache.http.conn.ssl.SSLSocketFactory;
 
@@ -22,13 +24,28 @@ import okhttp3.logging.HttpLoggingInterceptor;
  * okHttpClient
  */
 public class OkhttpProvidede {
-    static OkHttpClient okHttpClient;
+    static OkHttpClient okHttpClient,okHttpClientCheckToken;
     private static final int TIMEOUT_READ = 20;
     private static final int TIMEOUT_CONNECTION = 10;
     private static final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
     private static com.zkty.nativ.network.net.CacheInterceptor cacheInterceptor = new com.zkty.nativ.network.net.CacheInterceptor();
 
+
+
     public static OkHttpClient okHttpClient(boolean isCheckToken) {
+        if(isCheckToken){
+            return CheckToken();
+        }else{
+            return NotCheckToken();
+        }
+
+    }
+
+    /**
+     * 不检查token
+     * @return
+     */
+    public static OkHttpClient NotCheckToken(){
         if (okHttpClient == null) {
             synchronized (OkhttpProvidede.class) {
                 if (okHttpClient == null) {
@@ -43,15 +60,8 @@ public class OkhttpProvidede {
                     } catch (NoSuchAlgorithmException e) {
                         e.printStackTrace();
                     }
-
-
-                    OkHttpClient.Builder builder = new OkHttpClient.Builder();
-                    //是否拦截token
-                    if (isCheckToken) {
-                        builder.addInterceptor(NetworkMaster.getInstance().getTokenInterceptor());
-                    }
                     //SSL证书
-                    okHttpClient = builder.sslSocketFactory(new Tls12SocketFactory(sslContext.getSocketFactory()), new HttpsUtil.UnSafeTrustManager())
+                    okHttpClient = new OkHttpClient.Builder().sslSocketFactory(new Tls12SocketFactory(sslContext.getSocketFactory()), new HttpsUtil.UnSafeTrustManager())
                             .hostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
                             //打印日志
                             .addInterceptor(interceptor)
@@ -69,8 +79,52 @@ public class OkhttpProvidede {
                 }
             }
         }
-        return okHttpClient;
+         return okHttpClient;
+    }
 
+
+    /**
+     * 检查token
+     * @return
+     */
+    public static OkHttpClient CheckToken(){
+        if (okHttpClientCheckToken == null) {
+            synchronized (OkhttpProvidede.class) {
+                if (okHttpClientCheckToken == null) {
+                    SSLContext sslContext = null;
+                    try {
+                        sslContext = SSLContext.getInstance("TLS");
+                        try {
+                            sslContext.init(null, null, null);
+                        } catch (KeyManagementException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+
+                    //SSL证书
+                    okHttpClientCheckToken = new OkHttpClient.Builder().sslSocketFactory(new Tls12SocketFactory(sslContext.getSocketFactory()), new HttpsUtil.UnSafeTrustManager())
+                            //拦截token
+                            .hostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+                            .addInterceptor(NetworkMaster.getInstance().getTokenInterceptor())
+                            //打印日志
+                            .addInterceptor(interceptor)
+                            //设置Cache
+                            .addNetworkInterceptor(cacheInterceptor)//缓存方面需要加入这个拦截器
+                            .addInterceptor(cacheInterceptor)
+                            //time out
+                            .connectTimeout(TIMEOUT_CONNECTION, TimeUnit.SECONDS)
+                            .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
+                            .writeTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
+                            //失败重连
+                            .retryOnConnectionFailure(true)
+                            .build();
+
+                }
+            }
+        }
+        return okHttpClientCheckToken;
     }
 
 }

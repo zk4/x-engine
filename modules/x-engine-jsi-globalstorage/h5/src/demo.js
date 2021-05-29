4c4cd91e6c9ea33b750cb1472ad6e268d34b5f2a
MAJI <18510951471@163.com>
2021-05-29 18:46:13 +0800

    wip: 重新生成model.ts

--- modules/x-engine-jsi-globalstorage/h5/src/demo.js
+++ modules/x-engine-jsi-globalstorage/h5/src/demo.js
@@ -3,28 +3,28 @@ import globalstorage from './index.js'
 import xengine from "@zkty-team/x-engine-core";
 
 window.test_set = () => {
-{
+
   xengine.api("com.zkty.jsi.globalstorage", "set",{
     key:'abc',
     val:'world'
   });
-}}
+}
  document.getElementById("test_set").click()
       window.test_get = () => {
-{
+
   let val = xengine.api("com.zkty.jsi.globalstorage", "get",
     'abc',
   );
   document.getElementById("debug_text").innerText = val;
-}}
+}
  document.getElementById("test_get").click()
       window.test_del = () => {
-{
+
   xengine.api("com.zkty.jsi.globalstorage", "del",
     'abc',
   );
  
-}}
+}
  document.getElementById("test_del").click()
       
     

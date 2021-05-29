a2e45df95d3d130626d75f0bde9e4be7e9b83986
zk <liuzq7@gmail.com>
2021-05-29 23:51:37 +0800

    feat(script):  git msg 必须添加 scope

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -6,7 +6,7 @@ const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
 const releaseRE = /^v\d/
 const Merged = /^Merge /
-const commitRE = /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))+: .{1,50}/
 
 if (!Merged.test(msg) && !releaseRE.test(msg) && !commitRE.test(msg)) {
   console.log()
@@ -17,8 +17,8 @@ if (!Merged.test(msg) && !releaseRE.test(msg) && !commitRE.test(msg)) {
       chalk.red(
         `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
       ) +
-      `    ${chalk.green(`feat: add 'comments' option`)}\n` +
-      `    ${chalk.green(`fix: handle events on blur (close #28)`)}\n\n` +
+      `    ${chalk.green(`feat(xxx): add 'comments' option`)}\n` +
+      `    ${chalk.green(`fix(xxx): handle events on blur (close #28)`)}\n\n` +
       chalk.red(`  See .github/commit-convention.md for more details.\n`)
   )
   process.exit(1)

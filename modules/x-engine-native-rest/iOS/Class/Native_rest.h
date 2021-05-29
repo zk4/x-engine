0519de42c08fcc38ffdde738962bd56536cc5c7d
zk <liuzq7@gmail.com>
2021-05-29 19:01:00 +0800

    chore: compiable

--- modules/x-engine-native-rest/iOS/Class/Native_rest.h
+++ modules/x-engine-native-rest/iOS/Class/Native_rest.h
@@ -6,10 +6,10 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "NativeModule.h"
+#import "XENativeModule.h"
 #import "iRest.h"
 NS_ASSUME_NONNULL_BEGIN
-@interface Native_rest : NativeModule <iRest>
+@interface Native_rest : XENativeModule <iRest>
  
 @end
 

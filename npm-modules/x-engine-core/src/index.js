07925f77ecb648f74ecbcb1b2c01b2dc8c2d4195
wangsheng <wangsheng@u2finance.com>
2021-05-31 11:09:24 +0800

    feat: vue-engine-core 支持插件化

--- npm-modules/x-engine-core/src/index.js
+++ npm-modules/x-engine-core/src/index.js
@@ -243,42 +243,5 @@ patch.disableDoubleTapScroll = function (ms) {
     }
   }
 };
-export function install (Vue) {
-  Vue.prototype.$engine = xengine;
-  // 原生手机状态栏高度
-  Vue.prototype.$statusHeight = xengine.api(
-    "com.zkty.jsi.device",
-    "getStatusBarHeight"
-  );
-  // 原生手机导航条高度
-  Vue.prototype.$navigatorHeight = xengine.api(
-    "com.zkty.jsi.device",
-    "getNavigationHeight"
-  );
-  // 原生手机屏幕整体高度
-  Vue.prototype.$screenHeight = xengine.api(
-    "com.zkty.jsi.device",
-    "getScreenHeight"
-  );
-  // 原生手机底部tabbar高度
-  Vue.prototype.$tabbarHeight = xengine.api(
-    "com.zkty.jsi.device",
-    "getTabbarHeight"
-  );
-  // 原生手机相关信息
-  // this.$tabbarHeight.type          // iOS / android
-  // this.$tabbarHeight.systemVersion // 14.4
-  // this.$tabbarHeight.language      // en
-  // this.$tabbarHeight.UUID          // UUID number
-  xengine.api("com.zkty.jsi.device", "getDeviceInfo", {}, (val) => {
-    Vue.prototype.$deviceInfo = val;
-  });
-}
-export {
-  xengine
-}
-export default {
-  xengine,
-  install
-};
+export default xengine
 

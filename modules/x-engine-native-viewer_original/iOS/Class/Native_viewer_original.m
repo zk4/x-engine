dee9d65ac13865b09b0dd15353aed99e7d150242
Cwz-github <littlegrass003@gmail.com>
2021-06-07 16:33:07 +0800

    chore(viewer_original): 下载内容保存位置替换到caches文件夹下

--- modules/x-engine-native-viewer_original/iOS/Class/Native_viewer_original.m
+++ modules/x-engine-native-viewer_original/iOS/Class/Native_viewer_original.m
@@ -73,7 +73,12 @@ NATIVE_MODULE(Native_viewer_original)
     self.hud.userInteractionEnabled = YES;
     self.hud.removeFromSuperViewOnHide = YES;
     
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+
+    // document
+    //    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    // 
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
     NSString *localPath = [[paths objectAtIndex:0] stringByAppendingPathComponent:self.encryptUrl];
     NSFileManager *fileManager = [NSFileManager defaultManager];
     

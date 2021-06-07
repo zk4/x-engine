13bf58738a17a872d82485b8422137eafb9208eb
zk <liuzq7@gmail.com>
2021-06-08 03:53:07 +0800

    wip(hummer): runnable

---
+++ modules/x-engine-native-hummer/iOS/Class/Native_hummer.h
@@ -0,0 +1,14 @@
+//
+//  Native_hummer.h
+//  hummer
+//
+
+#import <Foundation/Foundation.h>
+#import "XENativeModule.h"
+#import "iHummer.h"
+NS_ASSUME_NONNULL_BEGIN
+@interface Native_hummer : XENativeModule <iHummer>
+ 
+@end
+
+NS_ASSUME_NONNULL_END

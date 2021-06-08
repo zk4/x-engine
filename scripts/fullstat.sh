7729de675d5f8f5ba33ff77342893f88b082a918
zk <liuzq7@gmail.com>
2021-06-08 10:52:33 +0800

    chore(stat): update stats

--- scripts/fullstat.sh
+++ scripts/fullstat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --author="$name" --pretty=tformat: --numstat | grep  -EI '.*\.(h|m|java|mm|ts|js|vue)$' | stat.py | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "+: %s, -: %s, =: %s\n", add, subs, loc }' -; done
+git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --author="$name" --pretty=tformat: --numstat | grep  -EI '.*\.(h|m|java|mm|ts|js|vue)$' | filter.py $name | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "+: %s, -: %s, =: %s\n", add, subs, loc }' -; done
--- scripts/simplestat.sh
+++ scripts/simplestat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git log --author="$1" --pretty=tformat: --numstat | grep -EI '.*\.(h|m|java|mm|ts|js)$' | stat.py | less
+git log --author="$1" --pretty=tformat: --numstat | grep -EI '.*\.(h|m|java|mm|ts|js|vue)$' | filter.py 'zk'| less

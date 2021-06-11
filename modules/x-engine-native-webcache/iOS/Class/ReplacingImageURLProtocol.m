000a6930e5a081fcc2da332741d5c4f9f2b59696
zk <liuzq7@gmail.com>
2021-06-12 03:15:57 +0800

    wip(webcache): alpha

--- modules/x-engine-native-webcache/iOS/Class/ReplacingImageURLProtocol.m
+++ modules/x-engine-native-webcache/iOS/Class/ReplacingImageURLProtocol.m
@@ -7,19 +7,23 @@
 //
 
 #import "ReplacingImageURLProtocol.h"
+#import "AFHTTPSessionManager.h"
 
 #import <UIKit/UIKit.h>
 
-static NSString* const FilteredKey = @"FilteredKey";
+static NSString* const FilteredKey = @"@@interceptable";
 
 @implementation ReplacingImageURLProtocol
 
+ 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request {
     NSString* extension = request.URL.pathExtension;
-    BOOL isImage = [@[@"png", @"jpeg", @"gif", @"jpg"] indexOfObjectPassingTest:^BOOL(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    BOOL interceptable = [@[@"js", @"css"] indexOfObjectPassingTest:^BOOL(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         return [extension compare:obj options:NSCaseInsensitiveSearch] == NSOrderedSame;
     }] != NSNotFound;
-    return [NSURLProtocol propertyForKey:FilteredKey inRequest:request] == nil && isImage;
+    return [NSURLProtocol propertyForKey:FilteredKey inRequest:request] == nil && interceptable;
+//    NSLog(@"==========>%@",request.URL);
+//    return NO;
 }
 
 + (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
@@ -30,11 +34,32 @@ static NSString* const FilteredKey = @"FilteredKey";
     NSMutableURLRequest* request = self.request.mutableCopy;
     [NSURLProtocol setProperty:@YES forKey:FilteredKey inRequest:request];
     
-    NSData* data = UIImagePNGRepresentation([UIImage imageNamed:@"image"]);
-    NSURLResponse* response = [[NSURLResponse alloc] initWithURL:self.request.URL MIMEType:@"image/png" expectedContentLength:data.length textEncodingName:nil];
-    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageAllowed];
-    [self.client URLProtocol:self didLoadData:data];
-    [self.client URLProtocolDidFinishLoading:self];
+    //1. 检测缓存
+    //   1. 无: 请求网络
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+    [manager
+     GET:self.request.URL.absoluteString
+     parameters:nil
+     headers:self.request.allHTTPHeaderFields
+     progress:nil
+     success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        // 准备数据
+        NSData* data = responseObject;
+        // TODO: MIMEType 应该用原始的
+        NSURLResponse* response = [[NSURLResponse alloc] initWithURL:self.request.URL MIMEType:@"application/*" expectedContentLength:data.length textEncodingName:nil];
+        [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageAllowed];
+        [self.client URLProtocol:self didLoadData:data];
+        [self.client URLProtocolDidFinishLoading:self];
+
+    }
+     failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error){
+        NSLog(@"failed");
+    }];
+    //   2. 有: 返回缓存
+    
+  
 }
 
 - (void)stopLoading {

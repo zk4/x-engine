0a673f00d272b47bf9f4f607079ec80a8e7fdfff
zk <liuzq7@gmail.com>
2021-07-23 01:21:01 +0800

    wip: formdata to bases64 beta

--- modules/x-engine-native-webcache/iOS/Class/ajaxhook.js
+++ modules/x-engine-native-webcache/iOS/Class/ajaxhook.js
@@ -1,20 +1,105 @@
+ 
 ;
 (function() {
+    // https://github.com/niklasvh/base64-arraybuffer/blob/master/lib/base64-arraybuffer.js
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
- 
+    // Use a lookup table to find the index.
+    var lookup = new Uint8Array(256);
+    for (var i = 0; i < chars.length; i++) {
+      lookup[chars.charCodeAt(i)] = i;
+    }
+
+    encode = function(arraybuffer) {
+      var bytes = new Uint8Array(arraybuffer),
+      i, len = bytes.length, base64 = "";
+
+      for (i = 0; i < len; i+=3) {
+        base64 += chars[bytes[i] >> 2];
+        base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+        base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+        base64 += chars[bytes[i + 2] & 63];
+      }
+
+      if ((len % 3) === 2) {
+        base64 = base64.substring(0, base64.length - 1) + "=";
+      } else if (len % 3 === 1) {
+        base64 = base64.substring(0, base64.length - 2) + "==";
+      }
+
+      return base64;
+    };
+
+    let decode =  function(base64) {
+      var bufferLength = base64.length * 0.75,
+      len = base64.length, i, p = 0,
+      encoded1, encoded2, encoded3, encoded4;
+
+      if (base64[base64.length - 1] === "=") {
+        bufferLength--;
+        if (base64[base64.length - 2] === "=") {
+          bufferLength--;
+        }
+      }
+
+      var arraybuffer = new ArrayBuffer(bufferLength),
+      bytes = new Uint8Array(arraybuffer);
+
+      for (i = 0; i < len; i+=4) {
+        encoded1 = lookup[base64.charCodeAt(i)];
+        encoded2 = lookup[base64.charCodeAt(i+1)];
+        encoded3 = lookup[base64.charCodeAt(i+2)];
+        encoded4 = lookup[base64.charCodeAt(i+3)];
+
+        bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
+        bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+        bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
+      }
+    }
+ ////////////////////////////////////////////////////////////////////////////////
     window.xengine_Ajax = {
         hookedXHR: {},
         hookAjax: hookAjax,
         nativeRequest: nativeRequest,
         nativeCallback: nativeCallback
     };
+    function formData2Json(formData){
+        let object = {};
+        formData.forEach((value, key) => {
+            if(key === 'image'){
+                let base64Img = encode(value.arrayBuffer());
+                console.log(base64Img);
+                
+                value.arrayBuffer().then(data=>{
+                    let base64Img = encode(data);
+                    console.log(base64Img);
+                    
+                });
+                return;
+            }
+            // Reflect.has in favor of: object.hasOwnProperty(key)
+            if(!Reflect.has(object, key)){
+                object[key] = value;
+                return;
+            }
+            if(!Array.isArray(object[key])){
+                object[key] = [object[key]];
+            }
+            object[key].push(value);
+        });
+        let json = JSON.stringify(object);
+        return json;
+    }
 
     function nativeRequest(xhrId, params) {
-       
+        if(FormData.prototype.isPrototypeOf(params.data)){
+            let json = formData2Json(params.data);
+            console.log("hell");
+        }
         //  请求 native
         params.xhrId = xhrId;
         if(window.dsBridge) {
-            window.dsBridge.call("com.zkty.jsi.webcache.xhrReuqest", params,function(data) {
+            window.dsBridge.call("com.zkty.jsi.webcache.xhrRequest", params,function(data) {
                 data = JSON.parse(data)
                 console.log(data)
 

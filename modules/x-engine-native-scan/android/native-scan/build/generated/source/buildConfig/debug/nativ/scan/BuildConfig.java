eced63cd150b689478eb552e1ef3dac45e13540a
MAJI <18510951471@163.com>
2021-07-28 18:42:38 +0800

    wip: 修改华为扫描二维码库

---
+++ modules/x-engine-native-scan/android/native-scan/build/generated/source/buildConfig/debug/nativ/scan/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nativ.scan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "nativ.scan";
+  public static final String BUILD_TYPE = "debug";
+}
--- modules/x-engine-native-scan/android/native-scan/src/main/java/com/zkty/nativ/scan/NativeScan.java
+++ modules/x-engine-native-scan/android/native-scan/src/main/java/com/zkty/nativ/scan/NativeScan.java
@@ -1,17 +1,24 @@
 package com.zkty.nativ.scan;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.huawei.hms.hmsscankit.ScanUtil;
+import com.huawei.hms.ml.scan.HmsScan;
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions;
 import com.zkty.nativ.core.NativeModule;
 import com.zkty.nativ.core.XEngineApplication;
 import com.zkty.nativ.jsi.view.BaseXEngineActivity;
 import com.zkty.nativ.jsi.view.LifecycleListener;
+import com.zkty.nativ.scan.activity.DefinedActivity;
 import com.zkty.nativ.scan.activity.ScanActivity;
 
 public class NativeScan extends NativeModule implements IScan {
@@ -21,6 +28,9 @@ public class NativeScan extends NativeModule implements IScan {
 
     private int REQUEST_CODE = 0x10;
     private LifecycleListener lifeCycleListener;
+    private int CODE_PERMISSION_CAMERA = 1;
+    private static final int REQUEST_CODE_DEFINE = 0X0111;
+    private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Override
     public String moduleId() {
@@ -86,7 +96,6 @@ public class NativeScan extends NativeModule implements IScan {
                     REQUEST_CODE++;
                     if (intent.hasExtra("result")) {
                         String code = intent.getStringExtra("result");
-                        Log.d(TAG, "scan result ::" + code);
                         if (!TextUtils.isEmpty(code)) {
                             callBack.succes(code);
 //                            if (mXEngineWebView != null) {
@@ -100,20 +109,51 @@ public class NativeScan extends NativeModule implements IScan {
                         }
                     }
                 }
+                if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_DEFINE) {
+                    HmsScan obj = intent.getParcelableExtra(ScanUtil.RESULT);
+                    if (obj != null) {
+                        callBack.succes(obj.getOriginalValue());
+                    }
+                    //MultiProcessor & Bitmap
+                }
             }
 
             @Override
-            public void onRequestPermissionsResult(int i, @NonNull String[] strings, @NonNull int[] ints) {
-
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] strings, @NonNull int[] ints) {
+                if (requestCode == CODE_PERMISSION_CAMERA) {
+                    if (activity.checkCallingOrSelfPermission(permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+                        startScan(activity);
+                    } else {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            activity.requestPermissions(permissions, CODE_PERMISSION_CAMERA);
+                        }
+                    }
+                }
             }
         };
 //        }
 
         act.addLifeCycleListener(lifeCycleListener);
 
-        Intent intent = new Intent();
-        intent.setClass(act.getApplicationContext(), ScanActivity.class);
-        act.startActivityForResult(intent, REQUEST_CODE);
 
+
+        if (activity.checkCallingOrSelfPermission(permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            startScan(activity);
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                activity.requestPermissions(permissions, CODE_PERMISSION_CAMERA);
+            }
+        }
+    }
+
+    private void startScan(Activity activity){
+        //zxing 扫描
+//        Intent intent = new Intent();
+//        intent.setClass(act.getApplicationContext(), ScanActivity.class);
+//        act.startActivityForResult(intent, REQUEST_CODE);
+        //华为默认样式
+        //            ScanUtil.startScan(activity, 0X01, new HmsScanAnalyzerOptions.Creator().create());
+        Intent intent = new Intent(activity, DefinedActivity.class);
+        activity.startActivityForResult(intent, REQUEST_CODE_DEFINE);
     }
 }

d35da6b5cc59c6c143420ef4639a1f009e55a96b
zk <liuzq7@gmail.com>
2021-07-29 15:22:44 +0800

    chore: update comment

--- modules/x-engine-native-tabbar/iOS/Class/iTabbarDelegate.h
+++ modules/x-engine-native-tabbar/iOS/Class/iTabbarDelegate.h
@@ -6,6 +6,7 @@
 #ifndef iTabbarDelegate_h
 #define iTabbarDelegate_h
 @protocol iTabbarDelegate <NSObject>
+// 当此方法调用时, 只需要返回当前你认为对的 tabItem 即可.
 - (UIViewController*)getCurrentTabItemVC;
 @end
 #endif /* iTabbar_h */

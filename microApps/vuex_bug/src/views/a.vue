8e36e763cd5698f3a753a0184324dc28f020996c
zk <liuzq7@gmail.com>
2021-08-17 12:56:38 +0800

    chore: 移动资源到 microapps

---
+++ microApps/vuex_bug/src/views/a.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        a页面测试count值：{{ $store.state.count }}
+        <br/>
+        <br/>
+        <button @click="handleClickLocation">跳转b页面</button>
+        <br/>
+        <br/>
+        <button @click="handleClickSetCount">改变当前值</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        
+        }
+    },
+    computed: {
+    
+    },
+    watch: {
+    
+    },
+    created() {
+    
+    },
+    mounted() {
+    
+    },
+    methods: {
+      onNativeShow() {
+        alert('当前页显示 ------ ')
+      },
+      handleClickLocation() {
+        this.$router.push({ path: '/b' })
+      },
+      handleClickSetCount() {
+        this.$store.commit('setCount')
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

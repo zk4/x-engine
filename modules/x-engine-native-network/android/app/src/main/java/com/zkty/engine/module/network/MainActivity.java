4c55f2da190883663e140cd9f388447cf0402e01
MAJI <18510951471@163.com>
2021-08-17 18:46:28 +0800

    wip: 选择图片上传

--- modules/x-engine-native-network/android/app/src/main/java/com/zkty/engine/module/network/MainActivity.java
+++ modules/x-engine-native-network/android/app/src/main/java/com/zkty/engine/module/network/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zkty.engine.module.network;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,17 +17,22 @@ import com.zkty.engine.module.network.bean.IMTokenInfoBean;
 import com.zkty.engine.module.network.bean.ImsessiionidBean;
 import com.zkty.engine.module.network.net.serve.NetworkServer;
 import com.zkty.engine.module.network.net.serve.RequestServer;
+import com.zkty.nativ.camera.cameraImpl.GlideLoader;
+import com.zkty.nativ.camera.cameraImpl.ImagePicker;
 import com.zkty.nativ.core.XEngineApplication;
+import com.zkty.nativ.core.utils.ImageUtils;
 import com.zkty.nativ.network.NetworkMaster;
 import com.zkty.nativ.network.bean.BaseResp;
 import com.zkty.nativ.network.net.exception.ApiException;
 import com.zkty.nativ.network.net.myinterface.OnDownloadListener;
+import com.zkty.nativ.network.net.myinterface.OnUploadListener;
 import com.zkty.nativ.network.net.myinterface.ServiceCallback;
 import com.zkty.nativ.network.utils.GsonUtil;
 
 import org.greenrobot.eventbus.EventBus;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,7 +40,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvMsg;
-    private Button btnDownLoad;
+    private Button btnDownLoad,btnUpLoad;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -47,7 +53,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btnPostBody).setOnClickListener(this);
         findViewById(R.id.btnPostQuery).setOnClickListener(this);
         btnDownLoad = findViewById(R.id.btnDownLoad);
+        btnUpLoad= findViewById(R.id.btnUpLoad);
         btnDownLoad.setOnClickListener(this);
+        btnUpLoad.setOnClickListener(this);
         tvMsg = findViewById(R.id.tvMsg);
     }
 
@@ -69,6 +77,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnDownLoad:
                 btnDownLoad.setEnabled(false);
                 download();
+                break;
+            case R.id.btnUpLoad:
+                btnUpLoad.setEnabled(false);
+                ImagePicker.getInstance()
+                        .setTitle("选择图片")//设置标题
+                        .showCamera(true)//设置是否显示拍照按钮
+                        .showImage(true)//设置是否展示图片
+                        .showVideo(false)//设置是否展示视频
+                        .filterGif(true)//设置是否过滤gif图片
+                        .setMaxCount(1)//设置最大选择图片数目(默认为1，单选)
+                        .setSingleType(true)//设置图片视频不能同时选择
+                        .setImageLoader(new GlideLoader())//设置自定义图片加载器
+                        .start(MainActivity.this, ImageUtils.RESULT_CODE_PHOTO);//REQEST_SELECT_IMAGES_CODE为Intent调用的requestCode
                 break;
         }
     }
@@ -199,6 +220,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * download 请求
      */
     private void download(){
+
         //创建文件夹
         File folder = new File(XEngineApplication.getCurrentActivity().getExternalCacheDir().getAbsoluteFile().getPath() + "/downloads");
         if (!folder.exists()) {
@@ -228,5 +250,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 tvMsg.setText(filePath + "\n下载失败");
             }
         });
+    }
+
+    /**
+     * 上传
+     */
+    private void upload(String filePath){
+        NetworkServer.getInstance().sendUpload("", filePath, new OnUploadListener() {
+            @Override
+            public void onUploadSuccess() {
+                tvMsg.setText(filePath + "\n上传完成");
+                btnUpLoad.setEnabled(true);
+            }
+
+            @Override
+            public void onUploading(int progress) {
+                tvMsg.setText(filePath + "\n" +progress + "%");
+
+            }
+
+            @Override
+            public void onUploadFailed() {
+                tvMsg.setText(filePath + "\n上传失败");
+                btnUpLoad.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case ImageUtils.RESULT_CODE_PHOTO:
+                    try {
+                        ArrayList<String> items = data.getStringArrayListExtra(ImagePicker.EXTRA_SELECT_IMAGES);
+                        //发送图片消息
+                        upload(items.get(0));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
     }
 }

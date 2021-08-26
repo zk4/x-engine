195c74060db4a90280830c4fa324f06764e59e01
Cwz-github <littlegrass003@gmail.com>
2021-08-26 18:58:49 +0800

    chore(media): 删除没用方法

--- modules/x-engine-jsi-media/iOS/Class/JSI_media.m
+++ modules/x-engine-jsi-media/iOS/Class/JSI_media.m
@@ -81,10 +81,4 @@ JSI_MODULE(JSI_media)
         }
     }];
 }
-
-- (NSString*)dictionaryToJson:(NSDictionary *)dic {
-    NSError *parseError = nil;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&parseError];
-    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-}
 @end

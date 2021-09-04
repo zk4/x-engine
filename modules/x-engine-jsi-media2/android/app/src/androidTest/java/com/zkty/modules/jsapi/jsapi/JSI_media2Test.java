f6470a6492583f451f2df75c6422608ef8fda414
MAJI <18510951471@163.com>
2021-09-04 18:35:13 +0800

    wip: 删除media2

--- modules/x-engine-jsi-media2/android/app/src/androidTest/java/com/zkty/modules/jsapi/jsapi/JSI_media2Test.java
+++
@@ -1,9 +0,0 @@
-package com.zkty.modules.jsapi.jsapi;
-
-import junit.framework.TestCase;
-
-public class JSI_media2Test extends TestCase {
-
-    public void test_setNavTitle() {
-    }
-}
--- modules/x-engine-native-media2/iOS/Class/iMedia2Delegate.h
+++
@@ -1,9 +0,0 @@
- 
-#ifndef iMedia2Delegate_h
-#define iMedia2Delegate_h
-#import <UIKit/UIKit.h>
-
-@protocol iMedia2Delegate <NSObject>
-- (void)sendUploadRequestWithUrl:(NSString *)url header:(NSDictionary *)header imageData:(NSData *)imageData imageName:(NSString *)imageName success:(void (^)(NSDictionary *dict))success failure:(void (^)(NSError *error))failure;
-@end
-#endif /* iMedia2_h */

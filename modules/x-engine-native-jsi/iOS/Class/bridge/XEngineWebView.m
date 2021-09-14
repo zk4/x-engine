5157799d2c9559e57ca4782a066b1953a884e567
Cwz-github <littlegrass003@gmail.com>
2021-09-14 16:10:11 +0800

    chore(x-engine-app): 去掉webview的手势

--- modules/x-engine-native-jsi/iOS/Class/bridge/XEngineWebView.m
+++ modules/x-engine-native-jsi/iOS/Class/bridge/XEngineWebView.m
@@ -81,18 +81,18 @@ typedef void (^XEngineCallBack)(id _Nullable result,BOOL complete);
 //    [[UIApplication sharedApplication].keyWindow.rootViewController.view addSubview: self.indicatorView];
 
     // 添加webview手势 如果recyleVc失效 就启用这个的
-    UISwipeGestureRecognizer *swiperGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleNavigationTransition:)];
-    swiperGesture.direction = UISwipeGestureRecognizerDirectionRight;
-    swiperGesture.delegate = self;
-    [self addGestureRecognizer:swiperGesture];
+//    UISwipeGestureRecognizer *swiperGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleNavigationTransition:)];
+//    swiperGesture.direction = UISwipeGestureRecognizerDirectionRight;
+//    swiperGesture.delegate = self;
+//    [self addGestureRecognizer:swiperGesture];
     
     return self;
 }
 
 // 一定要返回yes 让手势能往下传递
-- (BOOL)gestureRecognizer:(UIPanGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    return YES;
-}
+//- (BOOL)gestureRecognizer:(UIPanGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+//    return YES;
+//}
 
 - (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt
     defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame

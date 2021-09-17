f2f6b7944f6be1124e46f51fd6e934cd6615f7f2
zk <liuzq7@gmail.com>
2021-09-17 18:42:32 +0800

    chore: rollback

--- modules/x-engine-native-ui/iOS/Class/SafeGestureNavigationController.m
+++ modules/x-engine-native-ui/iOS/Class/SafeGestureNavigationController.m
@@ -11,121 +11,44 @@
 @end
 
 @implementation SafeGestureNavigationController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor =[UIColor clearColor];
     
-    self.navigationBar.backgroundColor = [UIColor whiteColor];
-    //设置全局右滑返回
-    [self setupRightPanReturn];
-    
-    [self.navigationItem setHidesBackButton:YES];
-    
-    self.delegate = self;
-}
-
-
-
-#pragma mark ---每次push之后生成返回按钮----
-- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
- 
-    // push的时候禁用手势
-    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.interactivePopGestureRecognizer.enabled = NO;
-        self.backGestureEnable = NO;
-    }
-    
-    [super pushViewController:viewController animated:animated];
-}
-
-
-- (void)setViewControllers:(NSArray<UIViewController *> *)viewControllers animated:(BOOL)animated{
-
-    // push的时候禁用手势
-    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.interactivePopGestureRecognizer.enabled = NO;
-        self.backGestureEnable = NO;
-    }
-    
-    [super setViewControllers:viewControllers animated:animated];
-}
-
-
-- (void)popViewController{
-    [self popViewControllerAnimated:YES];
-}
-
-
-- (UIViewController *)popViewControllerAnimated:(BOOL)animated{
-    // pop的时候禁用手势
-    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.interactivePopGestureRecognizer.enabled = NO;
-    }
-    
-    return [super popViewControllerAnimated:animated];
-}
-
-
-- (NSArray<UIViewController *> *)popToRootViewControllerAnimated:(BOOL)animated{
-
-    // pop的时候禁用手势
-    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        self.interactivePopGestureRecognizer.enabled = NO;
-    }
-    
-    return [super popToRootViewControllerAnimated:animated];
-}
-
-
-- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated{
-
-    // push完成后的时候判断是否在根控制器启用手势
-    if ([navigationController respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
-        
-        if ([navigationController.viewControllers count] == 1) {
-            navigationController.interactivePopGestureRecognizer.enabled = NO;
-        } else {
-            // 延迟手势启用，防止在 push 的时候同时 pop。
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                self.backGestureEnable = YES;
-                navigationController.interactivePopGestureRecognizer.enabled = YES;
-            });
-            
-        
-        }
-    }
-}
-
-
-#pragma mark ---处理全局右滑返回---
-- (void)setupRightPanReturn{
-
     // 获取系统自带滑动手势的target对象
     id target = self.interactivePopGestureRecognizer.delegate;
-    // 获取返回方法
-    SEL handler = NSSelectorFromString(@"handleNavigationTransition:");
-    //  获取添加系统边缘触发手势的View
-        UIView *targetView = self.interactivePopGestureRecognizer.view;
-    
-    //  创建pan手势 作用范围是全屏
-    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:target action:handler];
+    // 创建全屏滑动手势，调用系统自带滑动手势的target的action方法
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:target action:@selector(handleNavigationTransition:)];
+    // 设置手势代理，拦截手势触发
     pan.delegate = self;
-    [targetView addGestureRecognizer:pan];
-    
-    // 关闭边缘触发手势 防止和原有边缘手势冲突
-    [self.interactivePopGestureRecognizer setEnabled:NO];
+    // 给导航控制器的view添加全屏滑动手势
+    [self.view addGestureRecognizer:pan];
+    // 禁止使用系统自带的pop滑动手势
+    self.interactivePopGestureRecognizer.enabled = NO;
 }
+
+#pragma mark -- UIGestureRecognizerDelegate
 
 // 什么时候调用：每次触发手势之前都会询问下代理，是否触发。
 // 作用：拦截手势触发
-- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)gestureRecognizer{
-    
-    //解决与左滑手势冲突
-    CGPoint translation = [gestureRecognizer translationInView:gestureRecognizer.view];
-    if (translation.x <= 0 || !self.isBackGestureEnable) {
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    // 注意：只有非根控制器才有滑动返回功能，根控制器没有。
+    // 判断导航控制器是否只有一个子控制器，如果只有一个子控制器，肯定是根控制器
+    if (self.childViewControllers.count == 1 ) {
+        // 表示用户在根控制器界面，就不需要触发滑动手势，
         return NO;
     }
-    return self.childViewControllers.count == 1 ? NO : YES;
+    return YES;
 }
+//触发之后是否响应手势事件
+//处理侧滑返回与UISlider的拖动手势冲突
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    //如果手势是触摸的UISlider滑块触发的，侧滑返回手势就不响应
+    if ([touch.view isKindOfClass:[UISlider class]]) {
+        return NO;
+    }
+    return YES;
+}
+
   
 @end

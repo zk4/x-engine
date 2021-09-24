a7a088f7129a69964fb5ee79bbc3625baf36deab
zk <liuzq7@gmail.com>
2021-09-24 21:14:50 +0800

    chore: change back api name

--- modules/x-engine-native-jsi/iOS/Class/bridge/XEngineWebView.m
+++ modules/x-engine-native-jsi/iOS/Class/bridge/XEngineWebView.m
@@ -700,7 +700,7 @@ initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL))completi
 
 // 3- 确定下载的内容被允许之后再载入视图。
 - (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation{
-    [self triggerVueLifeCycleWithMethod:@"onWebLoaded"];
+    [self triggerVueLifeCycleWithMethod:@"onWebviewShow"];
     if(self.DSNavigationDelegate && [self.DSNavigationDelegate respondsToSelector:@selector(webView:didFinishNavigation:)]){
         [self.DSNavigationDelegate webView:webView didFinishNavigation:navigation];
     }

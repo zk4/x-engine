196baf610c0d948984eb0d4934dd94bdf1e68fb3
zk <liuzq7@gmail.com>
2021-09-25 00:23:46 +0800

    chore: rollback memory leak fix

--- modules/x-engine-native-geo_gaode/iOS/Class/FixMemoryLeak_geo_gaode.m
+++ modules/x-engine-native-geo_gaode/iOS/Class/FixMemoryLeak_geo_gaode.m
@@ -1,29 +1,29 @@
-//
-//#import "FixMemoryLeak_geo_gaode.h"
-//#import <SystemConfiguration/CaptiveNetwork.h>
-//#import "XTool.h"
-// 
-//@interface AMapStatistics (MemoryLeak_fix)
-//
-//@end
-//
-//@implementation AMapStatistics (MemoryLeak_fix)
-//
-//+(void)load{
-//    static dispatch_once_t onceToken;
-////    dispatch_once(&onceToken, ^{
-////        [XToolRuntime addInstanceFunc:[AMapStatistics class]
-////                                    fakeClass:[AMapStatistics class]
-////                                   withOldSel:@selector(fetchSSIDInfo)
-////                                   withNewSel:@selector(fetchSSIDInfo2)];
-////    });
-//}
-//
-//-(id)fetchSSIDInfo2
-//{
-//    // 这个高德的方法会内存泄漏 [AMapStatistics fetchSSIDInfo]
-//    // 原因 https://juejin.cn/post/6938305386684350495，拿 wifi 想做什么，不让拿！
-//    return nil ;
-//}
-//
-//@end
+
+#import "FixMemoryLeak_geo_gaode.h"
+#import <SystemConfiguration/CaptiveNetwork.h>
+#import "XTool.h"
+ 
+@interface AMapStatistics (MemoryLeak_fix)
+
+@end
+
+@implementation AMapStatistics (MemoryLeak_fix)
+
++(void)load{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [XToolRuntime addInstanceFunc:[AMapStatistics class]
+                                    fakeClass:[AMapStatistics class]
+                                   withOldSel:@selector(fetchSSIDInfo)
+                                   withNewSel:@selector(fetchSSIDInfo2)];
+    });
+}
+
+-(id)fetchSSIDInfo2
+{
+    // 这个高德的方法会内存泄漏 [AMapStatistics fetchSSIDInfo]
+    // 原因 https://juejin.cn/post/6938305386684350495，拿 wifi 想做什么，不让拿！
+    return nil ;
+}
+
+@end

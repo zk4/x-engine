9d1dc2f7a397b7b70d99899dcc1c6ea185bd923b
zk <liuzq7@gmail.com>
2021-10-12 14:55:05 +0800

    chore: fix bug

--- modules/x-engine-native-net/iOS/Class/filters/GlobalReqResMergeRequestFilter.m
+++ modules/x-engine-native-net/iOS/Class/filters/GlobalReqResMergeRequestFilter.m
@@ -36,7 +36,7 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sharedInstance = [[self alloc] init];
-        
+        sharedInstance.requests = [NSMutableDictionary new];
     });
     return sharedInstance;
 }

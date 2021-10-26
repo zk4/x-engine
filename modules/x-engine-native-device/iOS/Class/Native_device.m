f26826f7ad7cfee0c106eb6d4d3fb2e6f7559521
Cwz-github <littlegrass003@gmail.com>
2021-10-26 16:42:33 +0800

    chore(device): device添加判断手机型号的方法并返回

--- modules/x-engine-native-device/iOS/Class/Native_device.m
+++ modules/x-engine-native-device/iOS/Class/Native_device.m
@@ -9,6 +9,7 @@
 #import "Native_device.h"
 #import "XENativeContext.h"
 #import <x-engine-native-core/Unity.h>
+#import <sys/utsname.h>
 
 @interface Native_device()
 @end
@@ -61,17 +62,91 @@ NATIVE_MODULE(Native_device)
 }
 
 - (NSMutableDictionary *)getDeviceInfo {
-    NSString *systemName = [UIDevice currentDevice].systemName;
-    NSString *systemVersion = [UIDevice currentDevice].systemVersion;
     NSArray *languageArray = [NSLocale preferredLanguages];
     NSString *language = [languageArray objectAtIndex:0];
+    NSString *systemName = [UIDevice currentDevice].systemName;
+    NSString *systemVersion = [UIDevice currentDevice].systemVersion;
     NSString *UUID = [[UIDevice currentDevice] identifierForVendor].UUIDString;
+    NSString *photoType = [self phoneType];
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setValue:photoType forKey:@"photoType"];
     [dict setValue:systemName forKey:@"type"];
     [dict setValue:systemVersion forKey:@"systemVersion"];
     [dict setValue:language forKey:@"language"];
     [dict setValue:UUID forKey:@"UUID"];
     return dict;
 }
+
+- (NSString *)phoneType {
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
+    if ([platform isEqualToString:@"iPhone1,1"]) return @"iPhone 2G";
+    if ([platform isEqualToString:@"iPhone1,2"]) return @"iPhone 3G";
+    if ([platform isEqualToString:@"iPhone2,1"]) return @"iPhone 3GS";
+    if ([platform isEqualToString:@"iPhone3,1"]) return @"iPhone 4";
+    if ([platform isEqualToString:@"iPhone3,2"]) return @"iPhone 4";
+    if ([platform isEqualToString:@"iPhone3,3"]) return @"iPhone 4";
+    if ([platform isEqualToString:@"iPhone4,1"]) return @"iPhone 4S";
+    if ([platform isEqualToString:@"iPhone5,1"]) return @"iPhone 5";
+    if ([platform isEqualToString:@"iPhone5,2"]) return @"iPhone 5";
+    if ([platform isEqualToString:@"iPhone5,3"]) return @"iPhone 5c";
+    if ([platform isEqualToString:@"iPhone5,4"]) return @"iPhone 5c";
+    if ([platform isEqualToString:@"iPhone6,1"]) return @"iPhone 5s";
+    if ([platform isEqualToString:@"iPhone6,2"]) return @"iPhone 5s";
+    if ([platform isEqualToString:@"iPhone7,1"]) return @"iPhone 6 Plus";
+    if ([platform isEqualToString:@"iPhone7,2"]) return @"iPhone 6";
+    if ([platform isEqualToString:@"iPhone8,1"]) return @"iPhone 6s";
+    if ([platform isEqualToString:@"iPhone8,2"]) return @"iPhone 6s Plus";
+    if ([platform isEqualToString:@"iPhone8,4"]) return @"iPhone SE";
+    if ([platform isEqualToString:@"iPhone9,1"]) return @"iPhone 7";
+    if ([platform isEqualToString:@"iPhone9,2"]) return @"iPhone 7 Plus";
+    if([platform isEqualToString:@"iPhone10,1"]) return @"iPhone 8";
+    if([platform isEqualToString:@"iPhone10,4"]) return @"iPhone 8";
+    if([platform isEqualToString:@"iPhone10,2"]) return @"iPhone 8 Plus";
+    if([platform isEqualToString:@"iPhone10,5"]) return @"iPhone 8 Plus";
+    if([platform isEqualToString:@"iPhone10,3"]) return @"iPhone X";
+    if([platform isEqualToString:@"iPhone10,6"]) return @"iPhone X";
+    if ([platform isEqualToString:@"iPhone11,8"]) return @"iPhone XR";
+    if ([platform isEqualToString:@"iPhone11,2"]) return @"iPhone XS";
+    if ([platform isEqualToString:@"iPhone11,6"]) return @"iPhone XS Max";
+    if ([platform isEqualToString:@"iPhone11,4"]) return @"iPhone XS Max";
+    if ([platform isEqualToString:@"iPhone12,1"]) return @"iPhone 11";
+    if ([platform isEqualToString:@"iPhone12,3"]) return @"iPhone 11 Pro";
+    if ([platform isEqualToString:@"iPhone12,5"]) return @"iPhone 11 Pro Max";
+    if ([platform isEqualToString:@"iPhone12,8"]) return @"iPhone SE(2nd generation)";
+    if ([platform isEqualToString:@"iPhone13,1"]) return @"iPhone 12 mini";
+    if ([platform isEqualToString:@"iPhone13,2"]) return @"iPhone 12";
+    if ([platform isEqualToString:@"iPhone13,3"]) return @"iPhone 12 Pro";
+    if ([platform isEqualToString:@"iPhone13,4"]) return @"iPhone 12 Pro Max";
+    if ([platform isEqualToString:@"iPod1,1"]) return @"iPod Touch 1G";
+    if ([platform isEqualToString:@"iPod2,1"]) return @"iPod Touch 2G";
+    if ([platform isEqualToString:@"iPod3,1"]) return @"iPod Touch 3G";
+    if ([platform isEqualToString:@"iPod4,1"]) return @"iPod Touch 4G";
+    if ([platform isEqualToString:@"iPod5,1"]) return @"iPod Touch 5G";
+    if ([platform isEqualToString:@"iPad1,1"]) return @"iPad 1G";
+    if ([platform isEqualToString:@"iPad2,1"]) return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,2"]) return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,3"]) return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,4"]) return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,5"]) return @"iPad Mini 1G";
+    if ([platform isEqualToString:@"iPad2,6"]) return @"iPad Mini 1G";
+    if ([platform isEqualToString:@"iPad2,7"]) return @"iPad Mini 1G";
+    if ([platform isEqualToString:@"iPad3,1"]) return @"iPad 3";
+    if ([platform isEqualToString:@"iPad3,2"]) return @"iPad 3";
+    if ([platform isEqualToString:@"iPad3,3"]) return @"iPad 3";
+    if ([platform isEqualToString:@"iPad3,4"]) return @"iPad 4";
+    if ([platform isEqualToString:@"iPad3,5"]) return @"iPad 4";
+    if ([platform isEqualToString:@"iPad3,6"]) return @"iPad 4";
+    if ([platform isEqualToString:@"iPad4,1"]) return @"iPad Air";
+    if ([platform isEqualToString:@"iPad4,2"]) return @"iPad Air";
+    if ([platform isEqualToString:@"iPad4,3"]) return @"iPad Air";
+    if ([platform isEqualToString:@"iPad4,4"]) return @"iPad Mini 2G";
+    if ([platform isEqualToString:@"iPad4,5"]) return @"iPad Mini 2G";
+    if ([platform isEqualToString:@"iPad4,6"]) return @"iPad Mini 2G";
+    if ([platform isEqualToString:@"i386"]) return @"iPhone Simulator";
+    if ([platform isEqualToString:@"x86_64"]) return @"iPhone Simulator";
+    return platform;
+ }
 @end
  

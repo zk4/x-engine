491fcc1d89c10c9c55328d4ea08a0db0836f958d
zk <liuzq7@gmail.com>
2021-11-08 18:30:47 +0800

    chore: update docs

--- modules/x-engine-docs/docs/modules/all/模块-enviroment.md
+++ modules/x-engine-docs/docs/modules/all/模块-enviroment.md
@@ -12,7 +12,7 @@ version: 3.0.3
 
 ## isHybird
 [`sync`](/docs/modules/模块-规范?id=jsi-调用)
-
+ 这个方法其实只为了确定 jsi 能通。永远会返回 {code:0，msg:''}。
 
 **无参数**
 
@@ -21,8 +21,11 @@ version: 3.0.3
 ``` js
  {
 
+  // 如果没问题，code 为 0
+  // 有问题 code -1
   code: int,
 
+  // 问题描述
   msg:string
 
 }
--- modules/x-engine-jsi-enviroment/h5/src/demo.js
+++ modules/x-engine-jsi-enviroment/h5/src/demo.js
@@ -5,8 +5,9 @@ import xengine from "@zkty-team/x-engine-core";
 window.test_isHybrid = () => {
 
   let val = xengine.api("com.zkty.jsi.enviroment", "isHybird");
-  document.getElementById("debug_text").innerText = JSON.stringify(val);
-  xengine.assert('test_isHybrid', val.code ===0 && val.msg==="" )
+  console.log(val)
+  // document.getElementById("debug_text").innerText = JSON.stringify(val);
+  // xengine.assert('test_isHybrid', val.code ===0 && val.msg==="" )
 }
  document.getElementById("test_isHybrid").click()
         

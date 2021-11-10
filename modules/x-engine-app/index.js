b31b5b802588db94b739e973b65c4c60bf1410d4
Cwz-github <littlegrass003@gmail.com>
2021-11-10 20:57:53 +0800

    chore: rn finish

--- modules/x-engine-app/index.js
+++ modules/x-engine-app/index.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // sample : NativeModules.OpenNative && NativeModules.OpenNative.createCat("小花猫", "母", 3);
 const pushClick = () => {
-  NativeModules.DirectNative.pushNative("UIViewController");
+  NativeModules.RN_direct.pushNative();
 };
 
 const backClick = () => {
---
+++ modules/x-engine-rn-direct/iOS/direct/SwiftBridge.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftBridge.swift
+//  direct
+//
+//  Created by cwz on 2021/11/10.
+//  Copyright © 2021 zk. All rights reserved.
+//
+
+import UIKit
+
+class SwiftBridge: NSObject {
+
+}

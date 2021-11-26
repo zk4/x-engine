4d67d6bbb7afab7f6d3b9133253f58fce5ee7119
xiefei <xiefei_525@163.com>
2021-11-26 19:34:03 +0800

    chore: 修改定位

--- modules/x-engine-app/android/app/src/main/java/com/zkty/modules/engine/engine/application/ZKTYApplication.java
+++ modules/x-engine-app/android/app/src/main/java/com/zkty/modules/engine/engine/application/ZKTYApplication.java
@@ -2,7 +2,9 @@ package com.zkty.modules.engine.engine.application;
 
 
 import com.didichuxing.doraemonkit.DoraemonKit;
+import com.zkty.nativ.core.NativeContext;
 import com.zkty.nativ.core.XEngineApplication;
+import com.zkty.nativ.core.utils.Utils;
 
 
 public class ZKTYApplication extends XEngineApplication {
@@ -11,6 +13,9 @@ public class ZKTYApplication extends XEngineApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (Utils.getCurProcessName(this).equals(getApplicationInfo().packageName)) {
+            NativeContext.sharedInstance().init(this);
+        }
         DoraemonKit.install(this, "pId");
 
     }

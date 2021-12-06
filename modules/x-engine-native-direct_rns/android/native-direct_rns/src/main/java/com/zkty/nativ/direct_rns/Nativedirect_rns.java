afdf578fdbdc33ab3b312bf20198ddacef3f2f82
MAJI <18510951471@163.com>
2021-12-06 14:57:27 +0800

    wip: engine-2.0引入 direct_rn 模块

--- modules/x-engine-native-direct_rns/android/native-direct_rns/src/main/java/com/zkty/nativ/direct_rns/Nativedirect_rns.java
+++ modules/x-engine-native-direct_rns/android/native-direct_rns/src/main/java/com/zkty/nativ/direct_rns/Nativedirect_rns.java
@@ -3,20 +3,56 @@ package com.zkty.nativ.direct_rns;
 import android.util.Log;
 
 import com.tencent.mmkv.MMKV;
+import com.zkty.nativ.core.NativeContext;
 import com.zkty.nativ.core.NativeModule;
 import com.zkty.nativ.core.XEngineApplication;
+import com.zkty.nativ.direct.IDirect;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 
-public class Nativedirect_rns extends NativeModule implements Idirect_rns {
+public class Nativedirect_rns extends NativeModule implements IDirect {
+
+    private IDirect microappDirect;
 
     @Override
     public String moduleId() {
-        returns "com.zkty.native.direct_rns";
+        return "com.zkty.native.direct_rns";
     }
 
     @Override
     public void afterAllNativeModuleInited() {
+        List<NativeModule> modules = NativeContext.sharedInstance().getModulesByProtocol(IDirect.class);
 
+        for (NativeModule iDirect : modules) {
+            if (iDirect instanceof IDirect) {
+                if ("rns".equals(((IDirect) iDirect).scheme())) {
+                    this.microappDirect = (IDirect) iDirect;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String scheme() {
+        return "rns";
+    }
+
+    @Override
+    public String protocol() {
+        return "rns:";
+    }
+
+    @Override
+    public void push(String protocol, String host, String pathname, String fragment, Map<String, String> query, Map<String, String> params) {
+        if (microappDirect != null)
+            microappDirect.push(protocol(), host, pathname, fragment, query, params);
+    }
+
+    @Override
+    public void back(String host, String fragment) {
+        if (microappDirect != null)
+            microappDirect.back(host, fragment);
     }
 }

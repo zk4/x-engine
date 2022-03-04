6da4986eb13cea713219092eb20e7eacf8a6ada4
liuxiao00256 <m18291165921@163.com>
2022-03-04 18:30:33 +0800

    fix(x-engine): 修改自定义导航返回按钮样式

--- modules/x-engine-native-ui/iOS/Class/SafeGestureNavigationController.m
+++ modules/x-engine-native-ui/iOS/Class/SafeGestureNavigationController.m
@@ -25,6 +25,34 @@
 //    self.delegate = self;
 }
 
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    if (self.childViewControllers.count > 0) { // 如果push进来的不是第一个控制器
+        UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 80, 30)];
+        [button setImage:[UIImage imageNamed:@"appc_back"] forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:@"appc_back"] forState:UIControlStateHighlighted];
+        [button sizeToFit];
+        [button addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+        
+        // 隐藏tabbar
+        viewController.hidesBottomBarWhenPushed = YES;
+
+        // 保留滑动返回手势
+        if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
+            // 因为你把导航控制器自己的返回按钮覆盖了，所以这个代理会取消滑动返回功能，因此再覆盖前将此代理清空。再返回后再将代理还原。
+            self.interactivePopGestureRecognizer.delegate = nil;
+        }
+    }
+    
+    // 这句super的push要放在后面, 让viewController可以覆盖上面设置的leftBarButtonItem
+    [super pushViewController:viewController animated:animated];
+    
+}
+
+- (void)back {
+    [super popViewControllerAnimated:YES];
+}
+
 //
 //
 //#pragma mark ---每次push之后生成返回按钮----

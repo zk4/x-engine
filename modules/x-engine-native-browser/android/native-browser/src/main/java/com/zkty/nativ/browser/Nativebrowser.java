2d3458223fdcc5137fef0f340c547deba41bd9fa
xiefei <xiefei_525@163.com>
2022-06-20 16:37:30 +0800

    feature: 实现browser模块

--- modules/x-engine-native-browser/android/native-browser/src/main/java/com/zkty/nativ/browser/Nativebrowser.java
+++ modules/x-engine-native-browser/android/native-browser/src/main/java/com/zkty/nativ/browser/Nativebrowser.java
@@ -1,5 +1,7 @@
 package com.zkty.nativ.browser;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 import com.tencent.mmkv.MMKV;
@@ -17,6 +19,23 @@ public class Nativebrowser extends NativeModule implements Ibrowser {
 
     @Override
     public void afterAllNativeModuleInited() {
+
+    }
+
+    @Override
+    public void open(String url, CallBack callBack) {
+        try {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setData(Uri.parse(url));
+            XEngineApplication.getCurrentActivity().startActivity(intent);
+            callBack.onResult(0, "success");
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        callBack.onResult(-1, "链接解析失败，或找不到浏览器");
 
     }
 }

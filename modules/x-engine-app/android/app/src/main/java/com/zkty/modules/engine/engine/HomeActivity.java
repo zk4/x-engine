c2a20361adbcbfc5cc76920b2ea8acb4570b8595
xiefei <xiefei_525@163.com>
2022-07-01 17:41:00 +0800

    chore: 修改读取已安装应用判断方法

--- modules/x-engine-app/android/app/src/main/java/com/zkty/modules/engine/engine/HomeActivity.java
+++ modules/x-engine-app/android/app/src/main/java/com/zkty/modules/engine/engine/HomeActivity.java
@@ -30,7 +30,10 @@ import com.zkty.nativ.core.NativeModule;
 import com.zkty.nativ.core.XEngineApplication;
 import com.zkty.nativ.core.utils.PermissionsUtils;
 import com.zkty.nativ.core.utils.ToastUtils;
+import com.zkty.nativ.device.IDevice;
+import com.zkty.nativ.device.NativeDevice;
 import com.zkty.nativ.direct.DirectManager;
+import com.zkty.nativ.jsi.view.BaseXEngineActivity;
 import com.zkty.nativ.jsi.view.MicroAppsInstall;
 import com.zkty.nativ.jsi.view.XEngineWebActivityManager;
 import com.zkty.nativ.scan.activity.ScanActivity;
@@ -42,13 +45,9 @@ import com.zkty.nativ.viewer.Nativeviewer;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseXEngineActivity {
 
     private EditText et_content;
-    private PermissionsUtils permissionsUtils = new PermissionsUtils();
-    private String[] permissions = new String[]{
-
-            Manifest.permission.READ_CONTACTS};
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -98,62 +97,24 @@ public class HomeActivity extends AppCompatActivity {
 
             }
         }
-        if (resultCode == Activity.RESULT_OK && requestCode == 1) {
-            if (data != null) {
-                Uri uri = data.getData();
-                String phoneNum = null;
-                String contactName = null;
-                // 创建内容解析者
-                ContentResolver contentResolver = getContentResolver();
-                Cursor cursor = null;
-                if (uri != null) {
-                    cursor = contentResolver.query(uri,
-                            new String[]{"display_name","data1"}, null, null, null);
-                }
-                while (cursor.moveToNext()) {
-                    contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                    phoneNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                }
-                cursor.close();
-                //  把电话号码中的  -  符号 替换成空格
-                if (phoneNum != null) {
-                    phoneNum = phoneNum.replaceAll("-", " ");
-                    // 空格去掉  为什么不直接-替换成"" 因为测试的时候发现还是会有空格 只能这么处理
-                    phoneNum= phoneNum.replaceAll(" ", "");
-                }
 
-               Log.d("xxxxx","联系人：=="+contactName+"name"+phoneNum);
-                ToastUtils.showCenterToast("联系人：=="+contactName+"name"+phoneNum);
-            }
-
-        }
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        permissionsUtils.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
-    }
 
     public void nextAct(View view) {
 //        MicroAppsInstall.sharedInstance().init(XEngineApplication.getApplication());
-        permissionsUtils.checkPermissions(this, permissions, new PermissionsUtils.IPermissionsResult() {
-            @Override
-            public void passPermissions() {
-                Intent intent = new Intent();
-                intent.setAction("android.intent.action.PICK");
-                intent.addCategory("android.intent.category.DEFAULT");
-                intent.setType("vnd.android.cursor.dir/phone_v2");
-                startActivityForResult(intent, 1);
 
-            }
+        NativeModule module = NativeContext.sharedInstance().getModuleByProtocol(IDevice.class);
+        if (module instanceof NativeDevice) {
+            NativeDevice iView = (NativeDevice) module;
+            iView.pickContact((name, phone) -> {
+                ToastUtils.showCenterToast(name + "   :   " + phone);
+            });
 
-            @Override
-            public void forbidPermissions() {
+        }
 
-            }
-        });
+
     }
 
     public void download(View view) {

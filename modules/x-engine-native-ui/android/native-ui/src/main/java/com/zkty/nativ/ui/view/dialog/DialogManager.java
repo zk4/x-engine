d41d7520420e7a570c44deecb70b1756b8aeb911
xiefei <xiefei_525@163.com>
2022-07-28 17:54:14 +0800

    fix: 升级tbs

--- modules/x-engine-native-ui/android/native-ui/src/main/java/com/zkty/nativ/ui/view/dialog/DialogManager.java
+++ modules/x-engine-native-ui/android/native-ui/src/main/java/com/zkty/nativ/ui/view/dialog/DialogManager.java
@@ -11,7 +11,7 @@ public class DialogManager {
     //还未显示的dialog的list，这里是无序的
     private List<DialogBean> dialogList = new ArrayList<>();
 
-    //当前显示的dialog的list，最后一个就是显示在最上面的，是按优先级正序的。这里需要理解下，因为只有比上一个dialog优先级高的时候，才会显示并加入到这个list。
+    //当前显示的dialog的list.
     private List<DialogBean> currentDialogList = new ArrayList<>();
 
 
@@ -55,14 +55,11 @@ public class DialogManager {
             newDialog.getDialog().show();
             currentDialogList.add(newDialog);//加入到已显示的列表
         } else {//有弹窗显示时
-            if (newDialog.getPriority() > currentDialogList.get(currentDialogList.size() - 1).getPriority()) {//优先级高于当前显示的dialog
-                //显示新的dialog
-                newDialog.getDialog().show();
-                //之前显示的弹窗暂时隐藏掉
-                currentDialogList.get(currentDialogList.size() - 1).getDialogView().setVisibility(View.INVISIBLE);
-                currentDialogList.add(newDialog);//加入到已显示的列表
-            } else {//优先级和当前的dialog相等或者低于当前
-                dialogList.add(newDialog);//加入到待显示列表
+            //加入待显示列表
+            dialogList.add(newDialog);
+            //如果优先级比正在显示的大，关闭正在显示的
+            if (newDialog.getPriority() > currentDialogList.get(0).getPriority()) {//优先级高于当前显示的dialog
+                currentDialogList.get(0).dialog.build().dismiss();
             }
         }
     }
@@ -73,51 +70,47 @@ public class DialogManager {
     private void nextDialog() {
         //未显示列表里无dialog时
         if (dialogList.size() == 0) {
-            //显示列表不为空时
-            if (currentDialogList.size() > 0) {
-                //继续展示之前已经展示出来的
-                currentDialogList.get(currentDialogList.size() - 1).getDialogView().setVisibility(View.VISIBLE);
-            }
+
         }
         //未显示列表里有dialog时
         else {
-            //显示列表不为空时
-            if (currentDialogList.size() > 0) {
-                //1、先拿取已显示list里优先级最高的
-                DialogBean currentDialogBean = currentDialogList.get(currentDialogList.size() - 1);//最后一个就是优先级最高的
-                //2、再拿取未显示list里优先级最高的
-                DialogBean notShowDialogBean = dialogList.get(0);
-                for (int i = 0; i < dialogList.size(); i++) {
-                    if (notShowDialogBean.getPriority() < dialogList.get(i).getPriority()) {
-                        notShowDialogBean = dialogList.get(i);
-                    }
-                }
-                //3、对比出优先级最高的，如果优先级一样。那么依旧显示之前显示的
-                //4、显示优先级较高的
-                if (currentDialogBean.getPriority() < notShowDialogBean.getPriority()) {//未展示的优先级较高
-                    notShowDialogBean.getDialog().show();//展示未展示的
-                    currentDialogList.add(notShowDialogBean);//加入到展示列表里
-                    dialogList.remove(notShowDialogBean);//从未显示列表里删除
-                } else {
-                    //继续展示之前已经展示出来的
-                    currentDialogList.get(currentDialogList.size() - 1).getDialogView().setVisibility(View.VISIBLE);
-                }
-            } else {
-                //显示列表为空时
-                //1、再拿取未显示list里优先级最高的
-                DialogBean notShowDialogBean = dialogList.get(0);
-                for (int i = 0; i < dialogList.size(); i++) {
-                    if (notShowDialogBean.getPriority() < dialogList.get(i).getPriority()) {
-                        notShowDialogBean = dialogList.get(i);
-                    }
-                }
+//            //显示列表不为空时
+//            if (currentDialogList.size() > 0) {
+//                //1、先拿取已显示list里优先级最高的
+//                DialogBean currentDialogBean = currentDialogList.get(currentDialogList.size() - 1);//最后一个就是优先级最高的
+//                //2、再拿取未显示list里优先级最高的
+//                DialogBean notShowDialogBean = dialogList.get(0);
+//                for (int i = 0; i < dialogList.size(); i++) {
+//                    if (notShowDialogBean.getPriority() < dialogList.get(i).getPriority()) {
+//                        notShowDialogBean = dialogList.get(i);
+//                    }
+//                }
+//                //3、对比出优先级最高的，如果优先级一样。那么依旧显示之前显示的
+//                //4、显示优先级较高的
+//                if (currentDialogBean.getPriority() < notShowDialogBean.getPriority()) {//未展示的优先级较高
+//                    notShowDialogBean.getDialog().show();//展示未展示的
+//                    currentDialogList.add(notShowDialogBean);//加入到展示列表里
+//                    dialogList.remove(notShowDialogBean);//从未显示列表里删除
+//                } else {
+//                    //继续展示之前已经展示出来的
+//                    currentDialogList.get(currentDialogList.size() - 1).getDialogView().setVisibility(View.VISIBLE);
+//                }
+//            } else {
 
-                //2、显示优先级较高的
-                notShowDialogBean.getDialog().show();
-                currentDialogList.add(notShowDialogBean);//加入到展示列表里
-                dialogList.remove(notShowDialogBean);//从未显示列表里删除
+            //1、拿取未显示list里优先级最高的
+            DialogBean notShowDialogBean = dialogList.get(0);
+            for (int i = 0; i < dialogList.size(); i++) {
+                if (notShowDialogBean.getPriority() < dialogList.get(i).getPriority()) {
+                    notShowDialogBean = dialogList.get(i);
+                }
             }
+
+            //2、显示优先级较高的
+            notShowDialogBean.getDialog().show();
+            currentDialogList.add(notShowDialogBean);//加入到展示列表里
+            dialogList.remove(notShowDialogBean);//从未显示列表里删除
         }
+//        }
     }
 
     public static class DialogBean {
